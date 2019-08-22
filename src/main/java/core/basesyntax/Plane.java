006db081e463d91c13f3eac03a5212a6ad0b7b6e
Bohdan Chupika <boroda4436@users.noreply.github.com>
2019-08-22 15:56:45 +0300

    added test cases for builder pattern (#1)
    * added test cases for builder pattern
    
    * added test cases for builder pattern
    
    * fixed code style

---
+++ src/main/java/core/basesyntax/Plane.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+/**
+ * <p>Создайте свою собственную реализацию паттерна Builder для класса Plane.</p>
+ * <p>Добавьте не менее пяти полей в класс Plane.</p>
+ */
+
+public class Plane {
+
+}
