f72904bb52ad6eb2f4c2aa8a1c01a292f0f1f8cd
boroda4436 <chupika.bohdan@ukr.net>
2019-08-22 15:39:27 +0300

    fixed code style

--- src/test/java/core/basesyntax/PlaneTest.java
+++ src/test/java/core/basesyntax/PlaneTest.java
@@ -7,7 +7,11 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class PlaneTest {
     @Test
