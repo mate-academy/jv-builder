e71a76e297faeed73559182c83ead79581e07a24
Kokhanevich <nike.k1995@gmail.com>
2019-08-23 13:32:41 +0300

    added solution for HW5-Builder

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -6,5 +6,77 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String colour;
+    private String country;
+    private int maxSpeed;
+    private int maxWeight;
+    private int yearBuild;
+
+    public Plane(String name, String colour, String country, int maxSpeed,
+                 int maxWeight, int yearBuild) {
+        this.name = name;
+        this.colour = colour;
+        this.country = country;
+        this.maxSpeed = maxSpeed;
+        this.maxWeight = maxWeight;
+        this.yearBuild = yearBuild;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String country;
+        private String colour;
+        private int maxSpeed;
+        private int maxWeight;
+        private int yearBuild;
+
+        public PlaneBuilder() {
+
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setColour(String colour) {
+            this.colour = colour;
+            return this;
+        }
+
+        public PlaneBuilder setCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setMaxWeight(int maxWeight) {
+            this.maxWeight = maxWeight;
+            return this;
+        }
+
+        public PlaneBuilder setYearBuild(int yearBuild) {
+            this.yearBuild = yearBuild;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
+
+    private Plane(PlaneBuilder builder) {
+        name = builder.name;
+        colour = builder.colour;
+        country = builder.country;
+        maxSpeed = builder.maxSpeed;
+        maxWeight = builder.maxWeight;
+        yearBuild = builder.yearBuild;
+    }
 
 }
