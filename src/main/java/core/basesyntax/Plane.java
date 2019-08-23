cbd7c2f56e5e0fbf3dbdddc9771520317c84359b
Тарас <tarashryts@gmail.com>
2019-08-23 19:37:12 +0300

    add hw-52-builder

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -6,5 +6,62 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String type;
+    private Integer id;
+    private Integer speed;
+    private String color;
 
+    public Plane(String name, String type, Integer id, Integer speed, String color) {
+        this.name = name;
+        this.type = type;
+        this.id = id;
+        this.speed = speed;
+        this.color = color;
+    }
+
+    private Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.name;
+        this.type = planeBuilder.type;
+        this.id = planeBuilder.id;
+        this.speed = planeBuilder.speed;
+        this.color = planeBuilder.color;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String type;
+        private Integer id;
+        private Integer speed;
+        private String color;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(Integer speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
