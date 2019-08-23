089e0109d672e135f3739a0c51e4a972190546f7
roman_minevich <283389dv3qsh>
2019-08-23 16:39:10 +0300

    Added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,10 +1,60 @@
 package core.basesyntax;
-
 /**
- * <p>Создайте свою собственную реализацию паттерна Builder для класса Plane.</p>
- * <p>Добавьте не менее пяти полей в класс Plane.</p>
+ * Создайте свою собственную реализацию паттерна Builder для класса Plane.
+ * Добавьте не менее пяти полей в класс Plane.
  */
-
 public class Plane {
+    private String brand, color;
+    private int model, speed, productionYear;
 
+    public Plane(String brand, String color, int speed, int model, int productionYear) {
+        this.brand = brand;
+        this.color = color;
+        this.speed = speed;
+        this.model = model;
+        this.productionYear = productionYear;
+    }
+
+    private Plane(PlaneBuilder builder) {
+        this.brand = builder.brand;
+        this.color = builder.color;
+        this.speed = builder.speed;
+        this.model = builder.model;
+        this.productionYear = builder.productionYear;
+    }
+
+    public static class PlaneBuilder {
+
+        private String brand, color;
+        private int model, speed, productionYear;
+
+        public PlaneBuilder setBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setModel(int model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setProductionYear(int productionYear) {
+            this.productionYear = productionYear;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
