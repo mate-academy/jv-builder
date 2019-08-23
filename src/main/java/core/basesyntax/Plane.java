a03ad3026dc8e0e250e3bc2c70b3df52eaa12294
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-23 22:27:03 +0300

    added Builder pattern to Plane class

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -6,5 +6,63 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private int maxSpeed;
+    private int length;
+    private int width;
+    private int weight;
+
+    public Plane(String name, int maxSpeed, int length, int width, int weight) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+        this.length = length;
+        this.width = width;
+        this.weight = weight;
+    }
+
+    private Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.maxSpeed = builder.maxSpeed;
+        this.length = builder.length;
+        this.width = builder.width;
+        this.weight = builder.weight;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private int maxSpeed;
+        private int length;
+        private int width;
+        private int weight;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setLength(int length) {
+            this.length = length;
+            return this;
+        }
+
+        public PlaneBuilder setWidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 
 }
