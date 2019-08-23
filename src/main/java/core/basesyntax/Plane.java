c278165eab70779f673ca887737b2a45856ca2fd
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-23 12:40:52 +0300

    added solution for plane builder homework

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -6,5 +6,78 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int maxSpeed;
+    private String color;
+    private double length;
+    private double width;
+    private int engines;
 
+    private Plane(PlaneBuilder builder) {
+        maxSpeed = builder.maxSpeed;
+        color = builder.color;
+        length = builder.length;
+        width = builder.width;
+        engines = builder.engines;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public int getEngines() {
+        return engines;
+    }
+
+    static class PlaneBuilder {
+        private int maxSpeed;
+        private String color;
+        private double length;
+        private double width;
+        private int engines;
+
+        public PlaneBuilder(String color) {
+            this.color = color;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setLength(double length) {
+            this.length = length;
+            return this;
+        }
+
+        public PlaneBuilder setWidth(double width) {
+            this.width = width;
+            return this;
+        }
+
+        public PlaneBuilder setEngines(int engines) {
+            this.engines = engines;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
