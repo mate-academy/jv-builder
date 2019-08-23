c3839fc170bc4020cf2512f78f3eedb14548157f
kmety <kmety9876@gmail.com>
2019-08-23 22:55:26 +0300

    solution added

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -6,5 +6,63 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private double maxSpeed;
+    private double weight;
+    private double fuelTankCapacity;
+    private double maxDistance;
 
+    public Plane(String name, double maxSpeed, double weight,
+                 double fuelTankCapacity, double maxDistance) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+        this.weight = weight;
+        this.fuelTankCapacity = fuelTankCapacity;
+        this.maxDistance = maxDistance;
+    }
+
+    private Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.maxSpeed = builder.maxSpeed;
+        this.weight = builder.weight;
+        this.fuelTankCapacity = builder.fuelTankCapacity;
+        this.maxDistance = builder.maxDistance;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private double maxSpeed;
+        private double weight;
+        private double fuelTankCapacity;
+        private double maxDistance;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(double maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setFuelTankCapacity(double fuelTankCapacity) {
+            this.fuelTankCapacity = fuelTankCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setMaxDistance(double maxDistance) {
+            this.maxDistance = maxDistance;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
