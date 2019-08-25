55de7d63afb8e22287d7e17dc5d085f098e199d0
TatianaPirch <pirch.tatiana@gmail.com>
2019-08-25 11:31:46 +0300

    solution for PlaneBuilder

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -6,5 +6,63 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String model;
+    private int maxSpeed;
+    private int numberOfEngines;
+    private String type;
+    private int carryingCapacity;
 
+    public Plane(String model,int maxSpeed,int numberOfEngines,
+                 String type, int carryingCapacity) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.numberOfEngines = numberOfEngines;
+        this.type = type;
+        this.carryingCapacity = carryingCapacity;
+    }
+
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.maxSpeed = builder.maxSpeed;
+        this.numberOfEngines = builder.numberOfEngines;
+        this.type = builder.type;
+        this.carryingCapacity = builder.carryingCapacity;
+    }
+
+    public class PlaneBuilder {
+        private String model;
+        private int maxSpeed;
+        private int numberOfEngines;
+        private String type;
+        private int carryingCapacity;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfEngines(int numberOfEngines) {
+            this.numberOfEngines = numberOfEngines;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setCarryingCapacity(int carryingCapacity) {
+            this.carryingCapacity = carryingCapacity;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
