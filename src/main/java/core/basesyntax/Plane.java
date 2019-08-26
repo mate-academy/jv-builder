5d4ef6a5fd6ae7eb65a69075f192635bb0ca4a7f
Rosinskyi <sergey22061993@gmail.com>
2019-08-26 14:28:25 +0300

    added solution sor jv-builder

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -6,5 +6,90 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String color;
+    private int span;
+    private int tank;
+    private int weight;
 
+    public Plane(String name, String color, int span, int tank, int weight) {
+        this.name = name;
+        this.color = color;
+        this.span = span;
+        this.tank = tank;
+        this.weight = weight;
+    }
+
+    private Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.name;
+        this.color = planeBuilder.color;
+        this.span = planeBuilder.span;
+        this.tank = planeBuilder.tank;
+        this.weight = planeBuilder.weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getSpan() {
+        return span;
+    }
+
+    public int getTank() {
+        return tank;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+
+    public static class PlaneBuilder {
+        private String name;
+        private String color;
+        private int span;
+        private int tank;
+        private int weight;
+
+        public PlaneBuilder(String name) {
+            this.name = name;
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setSpan(int span) {
+            this.span = span;
+            return this;
+        }
+
+        public PlaneBuilder setTank(int tank) {
+            this.tank = tank;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+
+    }
 }
+
+
