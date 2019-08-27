0568cb16d9c7cbec6b7ec920f3ef2f0f54545dfb
Taras Khalak <tarasulo@gmail.com>
2019-08-27 15:23:00 +0300

    'Solution2'

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -4,7 +4,6 @@ package core.basesyntax;
  * <p>Создайте свою собственную реализацию паттерна Builder для класса Plane.</p>
  * <p>Добавьте не менее пяти полей в класс Plane.</p>
  */
-
 public class Plane {
     private String model;
     private String color;
