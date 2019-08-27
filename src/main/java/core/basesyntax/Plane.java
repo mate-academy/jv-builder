4bd0f6e966d28ea4318ff4befebc0140dc863783
Andrew <tolstuy1996@gmail.com>
2019-08-27 08:07:57 +0300

    made builder

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -6,5 +6,63 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private int maxSpeed;
+    private int yearOfIssue;
+    private int capacityPassegers;
+    private int capacityFuel;
 
+    public Plane(String model,int maxSpeed,int yearOfIssue,
+                 int capacityPassegers, int capacityFuel) {
+        this.name = model;
+        this.maxSpeed = maxSpeed;
+        this.yearOfIssue = yearOfIssue;
+        this.capacityPassegers = capacityPassegers;
+        this.capacityFuel = capacityFuel;
+    }
+
+    private Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.maxSpeed = builder.maxSpeed;
+        this.yearOfIssue = builder.yearOfIssue;
+        this.capacityPassegers = builder.capacityPassegers;
+        this.capacityFuel = builder.capacityFuel;
+    }
+
+    public class PlaneBuilder {
+        private String name;
+        private int maxSpeed;
+        private int yearOfIssue;
+        private int capacityPassegers;
+        private int capacityFuel;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setYearOfIssue(int yearOfIssue) {
+            this.yearOfIssue = yearOfIssue;
+            return this;
+        }
+
+        public PlaneBuilder setCapacityPassegers(int capacityPassegers) {
+            this.capacityPassegers = capacityPassegers;
+            return this;
+        }
+
+        public PlaneBuilder setCapacityFuel(int capacityFuel) {
+            this.capacityFuel = capacityFuel;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
