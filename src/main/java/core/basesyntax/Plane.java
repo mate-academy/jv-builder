a380dbe655167e0b8073ce8d29307429a74cdadb
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-08-27 14:34:21 +0300

    refactored

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -12,7 +12,50 @@ public class Plane {
     private int length;
     private String modelName;
 
-    private Plane() {
+    public static class PlaneBuilder {
+
+        private int seats;
+        private int maxSpeed;
+        private int weight;
+        private int length;
+        private String modelName;
+
+        public PlaneBuilder setSeats(int seats) {
+            this.seats = seats;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setLength(int length) {
+            this.length = length;
+            return this;
+        }
+
+        public PlaneBuilder setModelName(String modelName) {
+            this.modelName = modelName;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
+
+    private Plane(PlaneBuilder builder) {
+        seats = builder.seats;
+        maxSpeed = builder.maxSpeed;
+        weight = builder.weight;
+        length = builder.length;
+        modelName = builder.modelName;
     }
 
     public Plane(int seats, int maxSpeed, int weight, int length, String modelName) {
@@ -21,64 +64,5 @@ public class Plane {
         this.weight = weight;
         this.length = length;
         this.modelName = modelName;
-    }
-
-    public int getSeats() {
-        return seats;
-    }
-
-    public int getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public String getModelName() {
-        return modelName;
-    }
-
-    public static PlaneBuilder newBuilder() {
-        return new Plane().new PlaneBuilder();
-    }
-
-    public class PlaneBuilder {
-
-        private PlaneBuilder() {
-        }
-
-        public PlaneBuilder setSeats(int seats) {
-            Plane.this.seats = seats;
-            return this;
-        }
-
-        public PlaneBuilder setMaxSpeed(int maxSpeed) {
-            Plane.this.maxSpeed = maxSpeed;
-            return this;
-        }
-
-        public PlaneBuilder setWeight(int weight) {
-            Plane.this.weight = weight;
-            return this;
-        }
-
-        public PlaneBuilder setLength(int length) {
-            Plane.this.length = length;
-            return this;
-        }
-
-        public PlaneBuilder setModelName(String modelName) {
-            Plane.this.modelName = modelName;
-            return this;
-        }
-
-        public Plane build() {
-            return Plane.this;
-        }
     }
 }
