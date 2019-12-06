f26ffd7a84cd96fecf25c5c0489641def5d35739
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-06 16:37:39 +0200

    5.2 is done

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,55 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int modelNumber;
+    private int passangerCapacity;
+    private int width;
+    private double realeseYear;
+    private String color;
 
+    public Plane(int modelNumber, int passangerCapacity,
+                 int width, double realeseYear, String color) {
+        this.modelNumber = modelNumber;
+        this.passangerCapacity = passangerCapacity;
+        this.width = width;
+        this.realeseYear = realeseYear;
+        this.color = color;
+    }
+
+    public static class PlaneBuilder {
+        private int modelNumber;
+        private int passangerCapacity;
+        private int width;
+        private double realeseYear;
+        private String color;
+
+        public PlaneBuilder setModelNumber(int modelNumber) {
+            this.modelNumber = modelNumber;
+            return this;
+        }
+
+        public PlaneBuilder setPassangerCapacity(int passangerCapacity) {
+            this.passangerCapacity = passangerCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setWidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public PlaneBuilder setRealeseYear(double realeseYear) {
+            this.realeseYear = realeseYear;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(modelNumber, passangerCapacity, width, realeseYear, color);
+        }
+    }
 }
