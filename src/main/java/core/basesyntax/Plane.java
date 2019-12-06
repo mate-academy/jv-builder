46004c604ad0ebaea73275fa56387a53ca1eb057
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2019-12-06 16:57:07 +0200

    Added solution for hw 5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String model;
+    private int maxRange;
+    private int capacity;
+    private int age;
 
+    public Plane(String name, String model, int maxRange, int capacity, int age) {
+        this.name = name;
+        this.model = model;
+        this.maxRange = maxRange;
+        this.capacity = capacity;
+        this.age = age;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String model;
+        private int maxRange;
+        private int capacity;
+        private int age;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setMaxRange(int maxRange) {
+            this.maxRange = maxRange;
+            return this;
+        }
+
+        public PlaneBuilder setCapacity(int capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        public PlaneBuilder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(name, model, maxRange, capacity, age);
+        }
+    }
 }
