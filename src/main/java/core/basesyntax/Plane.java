036225e80a0deba8cc2793ffc809b2c16c0a3e6f
SergiyAgeev <sergiyageev@gmail.com>
2019-12-06 17:12:10 +0200

    hw 5 2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,63 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String markName;
+    private int maxSpeed;
+    private int maxFlyRange;
+    private int weight;
+    private boolean isBeautiful;
 
+    public Plane(String markName, int maxSpeed, int maxFlyRange, int weight, boolean isBeautiful) {
+        this.markName = markName;
+        this.maxSpeed = maxSpeed;
+        this.maxFlyRange = maxFlyRange;
+        this.weight = weight;
+        this.isBeautiful = isBeautiful;
+    }
+
+    private Plane(PlaneBuilder planeBuilder) {
+        this.markName = planeBuilder.markName;
+        this.maxFlyRange = planeBuilder.maxFlyRange;
+        this.weight = planeBuilder.weight;
+        this.maxSpeed = planeBuilder.maxSpeed;
+        this.isBeautiful = planeBuilder.isBeautiful;
+    }
+
+    public class PlaneBuilder {
+        private String markName;
+        private int maxSpeed;
+        private int maxFlyRange;
+        private int weight;
+        private boolean isBeautiful;
+
+        public PlaneBuilder setMarkName(String markName) {
+            this.markName = markName;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setMaxFlyRange(int maxFlyRange) {
+            this.maxFlyRange = maxFlyRange;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setIsBeautiful(boolean beautiful) {
+            isBeautiful = beautiful;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
+
