68ae7e0ef60f8592fe739ce0d3b33178dbabd158
Serhii Yevtushok <ggazila@gmail.com>
2019-12-06 15:05:14 +0200

    solution to hw5-2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,74 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String planeName;
+    private String companyName;
+    private int maxSpeed;
+    private int maxFuel;
+    private int seats;
+
+    public Plane(String planeName, String companyName, int maxSpeed, int maxFuel, int seats) {
+        this.planeName = planeName;
+        this.companyName = companyName;
+        this.maxSpeed = maxSpeed;
+        this.maxFuel = maxFuel;
+        this.seats = seats;
+    }
+
+    public String getPlaneName() {
+        return planeName;
+    }
+
+    public int getMaxFuel() {
+        return maxFuel;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public static class PlaneBuilder {
+        private String planeName;
+        private String companyName;
+        private int maxSpeed;
+        private int maxFuel;
+        private int seats;
+
+        public PlaneBuilder setPlaneName(String planeName) {
+            this.planeName = planeName;
+            return this;
+        }
+
+        public PlaneBuilder setMaxFuel(int maxFuel) {
+            this.maxFuel = maxFuel;
+            return this;
+        }
+
+        public PlaneBuilder setSeats(int seats) {
+            this.seats = seats;
+            return this;
+        }
+
+        public PlaneBuilder setCompanyName(String companyName) {
+            this.companyName = companyName;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(planeName, companyName, maxSpeed, maxFuel, seats);
+        }
+    }
 }
