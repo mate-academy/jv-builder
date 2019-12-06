f40d7ea906a9cbb8a833202550bbf9ce0bba8da4
Stanislav Karev <karsv.dvz@gmail.com>
2019-12-06 14:15:54 +0200

    add Builder pattern with Inner Class for Plane

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int length;
+    private int height;
+    private int width;
+    private double maxSpeed;
+    private String color;
 
+    public Plane(int length, int height, int width, double maxSpeed, String color) {
+        this.length = length;
+        this.height = height;
+        this.width = width;
+        this.maxSpeed = maxSpeed;
+        this.color = color;
+    }
+
+    public class PlaneBuilder {
+        private int length;
+        private int height;
+        private int width;
+        private double maxSpeed;
+        private String color;
+
+        public PlaneBuilder setLength(int length) {
+            this.length = length;
+            return this;
+        }
+
+        public PlaneBuilder setHeight(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public PlaneBuilder setWidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(double maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(length, height, width, maxSpeed, color);
+        }
+    }
 }
