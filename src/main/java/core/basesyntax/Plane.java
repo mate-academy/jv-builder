bb923c2b7bd9b8fd114162139dbaf6051a67a778
IvanKrivoruchko <krivoruchko.ivan.ukr.net>
2019-12-07 20:19:44 +0200

    Added hw52 solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,55 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private int weight;
+    private int length;
+    private int width;
+    private int numberOfSeats;
 
+    public Plane(String name, int weight, int length, int width, int numberOfSeats) {
+        this.name = name;
+        this.weight = weight;
+        this.length = length;
+        this.width = width;
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private int weight;
+        private int length;
+        private int width;
+        private int numberOfSeats;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setLength(int length) {
+            this.length = length;
+            return this;
+        }
+
+        public PlaneBuilder setWidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfSeats(int numberOfSeats) {
+            this.numberOfSeats = numberOfSeats;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(name, weight, length, width, numberOfSeats);
+        }
+
+    }
 }
