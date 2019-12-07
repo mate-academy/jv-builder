28d7e09ed9566d7d531ee7c7eaa08c9e6860c65d
Yaroslav <yaroslav2875@i.ua>
2019-12-07 14:04:21 +0200

    added hw 5.2 solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String model;
+    private int speed;
+    private int weight;
+    private int age;
+    private String color;
 
+    public Plane(String model, int speed, int weight, int age, String color) {
+        this.model = model;
+        this.speed = speed;
+        this.weight = weight;
+        this.age = age;
+        this.color = color;
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private int speed;
+        private int weight;
+        private int age;
+        private String color;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(model, speed, weight, age, color);
+        }
+    }
 }
