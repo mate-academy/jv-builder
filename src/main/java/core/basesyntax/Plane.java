472bb3637803387037278b51692b375b9f1c203d
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2019-12-09 00:59:35 +0200

    task done 2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,84 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String producer;
+    private String codeNumber;
+    private int maxSpeed;
+    private int quantityPassengers;
+    private boolean crashThisYear;
+
+    public Plane(String producer, String codeNumber, int maxSpeed, int quantityPassengers,
+                 boolean crashThisYear) {
+        this.producer = producer;
+        this.codeNumber = codeNumber;
+        this.maxSpeed = maxSpeed;
+        this.quantityPassengers = quantityPassengers;
+        this.crashThisYear = crashThisYear;
+    }
+
+    private Plane(PlaneBuilder builder) {
+        this.producer = builder.producer;
+        this.codeNumber = builder.codeNumber;
+        this.maxSpeed = builder.maxSpeed;
+        this.quantityPassengers = builder.quantityPassengers;
+        this.crashThisYear = builder.crashThisYear;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public String getCodeNumber() {
+        return codeNumber;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getQuantityPassengers() {
+        return quantityPassengers;
+    }
+
+    public boolean getCrashThisYear() {
+        return crashThisYear;
+    }
+
+    public static class PlaneBuilder {
+
+        private String producer;
+        private String codeNumber;
+        private int maxSpeed;
+        private int quantityPassengers;
+        private boolean crashThisYear;
+
+        public PlaneBuilder setProducer(String producer) {
+            this.producer = producer;
+            return this;
+        }
+
+        public PlaneBuilder setCodeNumber(String codeNumber) {
+            this.codeNumber = codeNumber;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setQuantityPassengers(int quantityPassengers) {
+            this.quantityPassengers = quantityPassengers;
+            return this;
+        }
+
+        public PlaneBuilder setCrashThisYear(boolean crashThisYear) {
+            this.crashThisYear = crashThisYear;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
