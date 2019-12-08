f3dfb9390771c593fbfcb770b56f9ea2c537b508
AlinaPodoliaka <57722396+AlinaPodoliaka@users.noreply.github.com>
2019-12-08 22:08:47 +0200

    added hw-5-2-solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,79 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String countryOfIssue;
+    private String model;
+    private int numberOfSeats;
+    private int cruisingSpeed;
+    private int maxFlightAltitude;
 
+    public Plane(String countryOfIssue, String model,
+                 int numberOfSeats, int cruisingSpeed, int maxFlightAltitude) {
+        this.countryOfIssue = countryOfIssue;
+        this.model = model;
+        this.numberOfSeats = numberOfSeats;
+        this.cruisingSpeed = cruisingSpeed;
+        this.maxFlightAltitude = maxFlightAltitude;
+
+    }
+
+    public String getCountryOfIssue() {
+        return countryOfIssue;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public int getCruisingSpeed() {
+        return cruisingSpeed;
+    }
+
+    public int getMaxFlightAltitude() {
+        return maxFlightAltitude;
+    }
+
+    public static class PlaneBuilder {
+
+        private String countryOfIssue;
+        private String model;
+        private int numberOfSeats;
+        private int cruisingSpeed;
+        private int maxFlightAltitude;
+
+        public PlaneBuilder setCountryOfIssue(String countryOfIssue) {
+
+            this.countryOfIssue = countryOfIssue;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfSeats(int numberOfSeats) {
+            this.numberOfSeats = numberOfSeats;
+            return this;
+        }
+
+        public PlaneBuilder setCruisingSpeed(int cruisingSpeed) {
+            this.cruisingSpeed = cruisingSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setMaxFlightAltitude(int maxFlightAltitude) {
+            this.maxFlightAltitude = maxFlightAltitude;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(countryOfIssue, model,
+                    numberOfSeats, cruisingSpeed, maxFlightAltitude);
+        }
+    }
 }
