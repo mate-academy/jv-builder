cbe7fc2a9bde1f1af32a0a3b433a410bafaa5f82
Grave <graveman13@gmail.com>
2019-12-08 18:21:56 +0300

    Home work 5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,85 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String type;
+    private String model;
+    private int numberOfSeats;
+    private int length;
+    private int height;
 
+    public Plane(String type, String model, int numberOfSeats, int length, int height) {
+        this.type = type;
+        this.model = model;
+        this.numberOfSeats = numberOfSeats;
+        this.length = length;
+        this.height = height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public class PlaneBuilder {
+
+        private String type;
+        private String model;
+        private int numberOfSeats;
+        private int length;
+        private int height;
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfSeats(int numberOfSeats) {
+            this.numberOfSeats = numberOfSeats;
+            return this;
+        }
+
+        public PlaneBuilder setLength(int length) {
+            this.length = length;
+            return this;
+        }
+
+        public PlaneBuilder setHeight(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this.type = type,
+                    this.model = model,
+                    this.numberOfSeats = numberOfSeats,
+                    this.length = length,
+                    this.height = height);
+        }
+    }
 }
+
+
+
+
+
+
