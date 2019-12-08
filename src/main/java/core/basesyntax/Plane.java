95e1e76ad7a7937bd24056b7fbd2b88395177434
Mykhailo Kramar <3805033@gmail.com>
2019-12-08 12:41:44 +0200

    hw5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,64 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String model;
+    private int engineCapacity;
+    private int passengerCapacity;
+    private int tankVolume;
 
+    public Plane(String name, String model, int engineCapacity,
+                 int passengerCapacity, int tankVolume) {
+        this.name = name;
+        this.model = model;
+        this.engineCapacity = engineCapacity;
+        this.passengerCapacity = passengerCapacity;
+        this.tankVolume = tankVolume;
+    }
+
+    private Plane(PlaneBuilder builder) {
+        this.name = name;
+        this.model = model;
+        this.engineCapacity = engineCapacity;
+        this.passengerCapacity = passengerCapacity;
+        this.tankVolume = tankVolume;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String model;
+        private int engineCapacity;
+        private int passengerCapacity;
+        private int tankVolume;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setPassengerCapacity(int passengerCapacity) {
+            this.passengerCapacity = passengerCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setTankVolume(int tankVolume) {
+            this.tankVolume = tankVolume;
+            return this;
+        }
+
+        public PlaneBuilder setEngineCapacity(int engineCapacity) {
+            this.engineCapacity = engineCapacity;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
+
