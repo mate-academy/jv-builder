49e297db8c4fb677d4a36a3181227ac685f32005
Riptod <otoval15>
2019-12-08 13:33:20 +0200

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String manufacturer;
+    private String model;
+    private String type;
+    private int speed;
+    private int rangeOfflight;
 
+    public Plane(String manufacturer,
+                 String model,
+                 String type,
+                 int speed,
+                 int rangeOfflight) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.type = type;
+        this.speed = speed;
+        this.rangeOfflight = rangeOfflight;
+    }
+
+    public static class PlaneBuilder {
+
+        private String manufacturer;
+        private String model;
+        private String type;
+        private int speed;
+        private int rangeOfflight;
+
+        public void setManufacturer(String manufacturer) {
+            this.manufacturer = manufacturer;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public void setSpeed(int speed) {
+            this.speed = speed;
+        }
+
+        public void setrangeOfflight(int rangeOfflight) {
+            this.rangeOfflight = rangeOfflight;
+        }
+
+        public Plane build() {
+            return new Plane(manufacturer, model, type, speed, rangeOfflight);
+        }
+    }
 }
