b04054603d05c41fc0183186dae151b3049402c3
Tsehelnyk <tsegelnikoff@gmail.com>
2019-12-09 13:42:13 +0200

    added solution method

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,86 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String model;
+    private String registrationNumber;
+    private int length;
+    private int height;
+    private int weight;
 
+    public Plane(String name, String model, String registrationNumber,
+                 int length, int height, int weight) {
+        this.name = name;
+        this.model = model;
+        this.registrationNumber = registrationNumber;
+        this.length = length;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public class PlaneBuilder {
+        private String name;
+        private String model;
+        private String registrationNumber;
+        private int length;
+        private int height;
+        private int weight;
+
+        private PlaneBuilder(String name) {
+            this.name = name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public void setRegistrationNumber(String registrationNumber) {
+            this.registrationNumber = registrationNumber;
+        }
+
+        public void setLength(int length) {
+            this.length = length;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public Plane build() {
+            return new Plane(name, model, registrationNumber,
+                    length, height, weight);
+        }
+    }
 }
