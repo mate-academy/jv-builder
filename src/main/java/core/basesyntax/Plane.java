7969199df3c33c1c9cda3e1bd6682750263fa57d
Alexandr Sashchenko <santys87@gmail.com>
2019-12-09 14:46:32 +0200

    Mate HW 5.2 Solution added

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,69 @@ package core.basesyntax;
 
 public class Plane {
 
+    private int enginesQuantity;
+    private int crewMembers;
+    private int rangeOfFlight;
+    private double weight;
+    private String identifier;
+
+    public Plane(
+        int enginesQuantity,
+        int crewMembers,
+        int rangeOfFlight,
+        double weight,
+        String identifier)
+    {
+        this.enginesQuantity = enginesQuantity;
+        this.crewMembers = crewMembers;
+        this.rangeOfFlight = rangeOfFlight;
+        this.weight = weight;
+        this.identifier = identifier;
+    }
+
+    private Plane(PlaneBuilder buildAPlane) {
+        this.enginesQuantity = buildAPlane.enginesQuantity;
+        this.crewMembers = buildAPlane.crewMembers;
+        this.rangeOfFlight = buildAPlane.rangeOfFlight;
+        this.weight = buildAPlane.weight;
+        this.identifier = buildAPlane.identifier;
+    }
+
+    public static class PlaneBuilder {
+
+        private int enginesQuantity;
+        private int crewMembers;
+        private int rangeOfFlight;
+        private double weight;
+        private String identifier;
+
+        public PlaneBuilder setEnginesQuantity(int enginesQuantity) {
+            this.enginesQuantity = enginesQuantity;
+            return this;
+        }
+
+        public PlaneBuilder setRangeOfFlight(int crewMembers) {
+            this.crewMembers = crewMembers;
+            return this;
+        }
+
+        public PlaneBuilder setCrewMembers(int rangeOfFlight) {
+            this.rangeOfFlight = rangeOfFlight;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setIdentifier(String identifier) {
+            this.identifier = identifier;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
