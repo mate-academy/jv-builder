9fdf6da6b39e8f781e93d8229bf4d18c5119a6e5
petrukillya <petrukillya98@gmail.com>
2020-04-02 21:15:48 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,55 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String color;
+    private String model;
+    private int cruisingSpeed;
+    private double maxFlightRange;
 
+    private Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.color = builder.color;
+        this.model = builder.model;
+        this.cruisingSpeed = builder.cruisingSpeed;
+        this.maxFlightRange = builder.maxFlightRange;
+    }
+
+    public static class PlaneBuilder {
+
+        private String name;
+        private String color;
+        private String model;
+        private int cruisingSpeed;
+        private double maxFlightRange;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setCruisingSpeed(int cruisingSpeed) {
+            this.cruisingSpeed = cruisingSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setMaxFlightRange(double maxFlightRange) {
+            this.maxFlightRange = maxFlightRange;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
