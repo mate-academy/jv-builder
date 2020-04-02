e7d14e348ab70c1332e73a3654a8f87cc5f0de3f
Galina <gala6ap@mail.ru>
2020-04-02 14:20:29 +0300

    Plane and PlaneBuilder classes implemented

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,74 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private final String type;
+    private final String producer;
+    private final String model;
+    private final int seatsNumber;
+    private final int productionYear;
 
+    public Plane(PlaneBuilder builder) {
+        type = builder.planeType;
+        producer = builder.planeProducer;
+        model = builder.planeModel;
+        seatsNumber = builder.seatsNumberInPlane;
+        productionYear = builder.planeProductionYear;
+    }
+
+    public static class PlaneBuilder {
+        private String planeType;
+        private String planeProducer;
+        private String planeModel;
+        private int seatsNumberInPlane;
+        private int planeProductionYear;
+
+        public PlaneBuilder setType(String type) {
+            planeType = type;
+            return this;
+        }
+
+        public PlaneBuilder setProducer(String producer) {
+            planeProducer = producer;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            planeModel = model;
+            return this;
+        }
+
+        public PlaneBuilder setSeatsNumber(int seatsNumber) {
+            seatsNumberInPlane = seatsNumber;
+            return this;
+        }
+
+        public PlaneBuilder setProductionYear(int productionYear) {
+            planeProductionYear = productionYear;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeatsNumber() {
+        return seatsNumber;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
 }
