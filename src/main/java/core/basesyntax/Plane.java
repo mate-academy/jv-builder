c9b81b616084ae7d79ec1fb7937d3869517ab7bf
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-02 16:58:14 +0300

    internal commit. added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,58 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String model;
+    private String vendor;
+    private String vendorCountry;
+    private String serialNumber;
+    private long mileage;
 
+    public Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.vendor = builder.vendor;
+        this.vendorCountry = builder.vendorCountry;
+        this.serialNumber = builder.serialNumber;
+        this.mileage = builder.mileage;
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private String vendor;
+        private String vendorCountry;
+        private String serialNumber;
+        private long mileage;
+
+        public PlaneBuilder(String model) {
+            this.model = model;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setVendor(String vendor) {
+            this.vendor = vendor;
+            return this;
+        }
+
+        public PlaneBuilder setVendorCountry(String vendorCountry) {
+            this.vendorCountry = vendorCountry;
+            return this;
+        }
+
+        public PlaneBuilder setSerialNumber(String serialNumber) {
+            this.serialNumber = serialNumber;
+            return this;
+        }
+
+        public PlaneBuilder setMileage(long mileage) {
+            this.mileage = mileage;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
