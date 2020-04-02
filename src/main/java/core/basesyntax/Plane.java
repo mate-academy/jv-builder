b384a729e2591b3716379931523619a165bde2a5
Vitamin-oht <61590116+Vitamin-oht@users.noreply.github.com>
2020-04-02 21:11:15 +0300

    Builder created

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,78 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String type;
+    private String color;
+    private int maxSpeed;
+    private int maxHigh;
 
+    private Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.type = builder.type;
+        this.color = builder.color;
+        this.maxSpeed = builder.maxSpeed;
+        this.maxHigh = builder.maxHigh;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getMaxHigh() {
+        return maxHigh;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String type;
+        private String color;
+        private int maxSpeed;
+        private int maxHigh;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setMaxHigh(int maxHigh) {
+            this.maxHigh = maxHigh;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+
+        }
+    }
 }
+
+
+
