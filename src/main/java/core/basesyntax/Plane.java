d9e498485f118c58680e210fc15e693f1b235fe3
andriy <maziarandriy@gmail.com>
2020-04-02 15:37:38 +0300

    hw5.2 solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,87 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String type;
+    private double lengthOfPlane;
+    private int amountEngine;
+    private int maxSpeed;
 
+    public Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.type = builder.type;
+        this.lengthOfPlane = builder.lengthOfPlane;
+        this.amountEngine = builder.amountEngine;
+        this.maxSpeed = builder.maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{name: " + name
+                + "type: " + type
+                + "length of Plane: " + lengthOfPlane
+                + "amount of engine: " + amountEngine
+                + "max speed: " + maxSpeed + '}';
+    }
+
+    public String getName(String name) {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getLengthOfPlane() {
+        return lengthOfPlane;
+    }
+
+    public int getAmountEngine() {
+        return amountEngine;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public class PlaneBuilder {
+        private String name;
+        private String type;
+        private double lengthOfPlane;
+        private int amountEngine;
+        private int maxSpeed;
+
+        public PlaneBuilder(String name) {
+            this.name = name;
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.type = name;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setLengthOfPlane(double lengthOfPlane) {
+            this.lengthOfPlane = lengthOfPlane;
+            return this;
+        }
+
+        public PlaneBuilder setAmountEngine(int amountEngine) {
+            this.amountEngine = amountEngine;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
