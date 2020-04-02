3341b3c3418eff3b5f60d1bf8772d4a38f6990c9
Ludchenko <skillet367@icloud.com>
2020-04-02 21:11:23 +0300

    added Builder and it's methods, together with main class

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,50 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String planeModel;
+    private String planeEngineType;
+    private int planeSize;
+    private int planeSpeed;
+    private PlaneBuilder planeBuilder;
 
+    private Plane(PlaneBuilder planeBuilder) {
+        this.planeBuilder = planeBuilder;
+    }
+
+    public class PlaneBuilder {
+        public PlaneBuilder setPlaneModel(String planeModel) {
+
+            Plane.this.planeModel = planeModel;
+
+            return this;
+        }
+
+        public PlaneBuilder setPlaneEngineType(String planeEngineType) {
+            Plane.this.planeEngineType = planeEngineType;
+
+            return this;
+        }
+
+        public PlaneBuilder setPlaneSize(int planeSize) {
+            Plane.this.planeSize = planeSize;
+
+            return this;
+        }
+
+        public PlaneBuilder setPlaneSpeed(int planeSpeed) {
+            Plane.this.planeSpeed = planeSpeed;
+
+            return this;
+        }
+
+        public PlaneBuilder setPlaneBuilder(PlaneBuilder planeBuilder) {
+            Plane.this.planeBuilder = planeBuilder;
+
+            return this;
+        }
+
+        public Plane build() {
+            return Plane.this;
+        }
+    }
 }
