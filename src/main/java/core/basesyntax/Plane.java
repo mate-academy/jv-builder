13264a9fb6f19207f2b0f42b4e4de14c6e16fce0
Yaroslav-Serhii <u.vusochanskiy@gmail.com>
2020-04-02 13:16:13 +0300

    added solution.

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int size;
+    private String model;
+    private String number;
+    private long price;
+    private int age;
 
+    private Plane(PlaneBuilder pb) {
+        pb.build();
+    }
+
+    public class PlaneBuilder {
+
+        private PlaneBuilder() {
+
+        }
+
+        public PlaneBuilder setsize(int size) {
+            Plane.this.size = size;
+
+            return this;
+        }
+
+        public PlaneBuilder setmodel(String model) {
+            Plane.this.model = model;
+
+            return this;
+        }
+
+        public PlaneBuilder setnumber(String number) {
+            Plane.this.number = number;
+
+            return this;
+        }
+
+        public PlaneBuilder setage(int age) {
+            Plane.this.age = age;
+
+            return this;
+        }
+
+        public PlaneBuilder setprice(long price) {
+            Plane.this.price = price;
+
+            return this;
+        }
+
+        public Plane build() {
+            return Plane.this;
+        }
+    }
 }
