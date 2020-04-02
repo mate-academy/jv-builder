51b8693a1e510f199b17aa0dd7dcc7f47d5691ea
Allduiin <vovapro01@gmail.com>
2020-04-02 16:49:47 +0300

    Added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,58 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String color;
+    private String configuration;
+    private String serialNumber;
+    private int power;
+    private int maxSpeed;
 
+    public Plane(PlaneBuilder builder) {
+        this.color = builder.color;
+        this.configuration = builder.configuration;
+        this.serialNumber = builder.serialNumber;
+        this.power = builder.power;
+        this.maxSpeed = builder.maxSpeed;
+    }
+
+    public static class PlaneBuilder {
+
+        private String color;
+        private String configuration;
+        private String serialNumber;
+        private int power;
+        private int maxSpeed;
+
+        public PlaneBuilder() {
+        }
+
+        public PlaneBuilder setConfiguration(String configuration) {
+            this.configuration = configuration;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setSerialNumber(String serialNumber) {
+            this.serialNumber = serialNumber;
+            return this;
+        }
+
+        public PlaneBuilder setPower(int power) {
+            this.power = power;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
