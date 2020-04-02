539368b88a26bd3167ddac92616868fcb9f1e287
Djeckkenni <djeck27kenni@gmail.com>
2020-04-03 00:05:34 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,75 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int numberOfPlaces;
+    private int cruisingSpeed;
+    private int rangeOfFlight;
+    private int maximumWeight;
+    private String course;
 
+    private Plane(PlaneBuilder planeBuilder) {
+        this.numberOfPlaces = planeBuilder.numberOfPlaces;
+        this.cruisingSpeed = planeBuilder.cruisingSpeed;
+        this.rangeOfFlight = planeBuilder.rangeOfFlight;
+        this.maximumWeight = planeBuilder.maximumWeight;
+        this.course = planeBuilder.course;
+    }
+
+    public int getNumberOfPlaces() {
+        return numberOfPlaces;
+    }
+
+    public int getCruisingSpeed() {
+        return cruisingSpeed;
+    }
+
+    public int getRangeOfFlight() {
+        return rangeOfFlight;
+    }
+
+    public int getMaximumWeight() {
+        return maximumWeight;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public static class PlaneBuilder {
+        private int numberOfPlaces;
+        private int cruisingSpeed;
+        private int rangeOfFlight;
+        private int maximumWeight;
+        private String course;
+
+        public PlaneBuilder setCourse(String course) {
+            this.course = course;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfPlaces(int numberOfPlaces) {
+            this.numberOfPlaces = numberOfPlaces;
+            return this;
+        }
+
+        public PlaneBuilder setCruisingSpeed(int cruisingSpeed) {
+            this.cruisingSpeed = cruisingSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setRangeOfFlight(int rangeOfFlight) {
+            this.rangeOfFlight = rangeOfFlight;
+            return this;
+        }
+
+        public PlaneBuilder setMaximumWeight(int maximumWeight) {
+            this.maximumWeight = maximumWeight;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
+
