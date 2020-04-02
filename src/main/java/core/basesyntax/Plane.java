ae570d7ec3a0607d169828dc8ce58fcab7487864
maks45 <maksim.durov45@gmail.com>
2020-04-02 14:31:50 +0300

    add solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,57 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int weight;
+    private int maxSpeed;
+    private int passengersCount;
+    private String colour;
+    private String brand;
 
+    private Plane(PlaneBuilder builder) {
+        this.weight = builder.weight;
+        this.maxSpeed = builder.maxSpeed;
+        this.passengersCount = builder.passengersCount;
+        this.colour = builder.colour;
+        this.brand = builder.brand;
+    }
+
+    public static class PlaneBuilder {
+        private int weight;
+        private int maxSpeed;
+        private int passengersCount;
+        private String colour;
+        private String brand;
+
+        public PlaneBuilder() {
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setPassengersCount(int passengersCount) {
+            this.passengersCount = passengersCount;
+            return this;
+        }
+
+        public PlaneBuilder setColour(String colour) {
+            this.colour = colour;
+            return this;
+        }
+
+        public PlaneBuilder setBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
