87d6cbc68cf6313ad738038c3dcb16e446a8c3d5
Nick97-git <nickarchangel97@gmail.com>
2020-04-02 12:44:19 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,79 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String model;
+    private String color;
+    private String typeOfEngine;
+    private int numOfEngine;
 
+    private Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.model = builder.model;
+        this.color = builder.color;
+        this.typeOfEngine = builder.typeOfEngine;
+        this.numOfEngine = builder.numOfEngine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getTypeOfEngine() {
+        return typeOfEngine;
+    }
+
+    public int getNumOfEngine() {
+        return numOfEngine;
+    }
+
+    public static class PlaneBuilder {
+
+        private String name;
+        private String model;
+        private String color;
+        private String typeOfEngine;
+        private int numOfEngine;
+
+        public PlaneBuilder(String name) {
+            this.name = name;
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setTypeOfEngine(String typeOfEngine) {
+            this.typeOfEngine = typeOfEngine;
+            return this;
+        }
+
+        public PlaneBuilder setNumOfEngine(int numOfEngine) {
+            this.numOfEngine = numOfEngine;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
