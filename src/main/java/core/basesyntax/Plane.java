bae22648b42eff5c426d1578622110ef6965ae67
Andrey Tanchak <tanchak12@gmail.com>
2020-04-02 14:15:01 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,74 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int speed;
+    private int weight;
+    private String model;
+    private int places;
+    private int rangeOfFlight;
 
+    private Plane(PlaneBuilder builder) {
+        this.speed = builder.speed;
+        this.weight = builder.weight;
+        this.rangeOfFlight = builder.rangeOfFlight;
+        this.places = builder.places;
+        this.model = builder.model;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPlaces() {
+        return places;
+    }
+
+    public int getRangeOfFlight() {
+        return rangeOfFlight;
+    }
+
+    public static class PlaneBuilder {
+        private int speed;
+        private int weight;
+        private int places;
+        private int rangeOfFlight;
+        private String model;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setPlaces(int places) {
+            this.places = places;
+            return this;
+        }
+
+        public PlaneBuilder setRangeOfFlight(int rangeOfFlight) {
+            this.rangeOfFlight = rangeOfFlight;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
