004f12ec5e8ac67b87f4868b9e26c535e9e0e831
Богдан Демьянчук <t0974447144@gmail.com>
2020-04-02 15:24:15 +0300

    Soluton WH 5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,74 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String mark;
+    private String name;
+    private int seats;
+    private double length;
+    private boolean flies;
 
+    private Plane(PlaneBuilder builder) {
+        this.mark = builder.mark;
+        this.name = builder.name;
+        this.seats = builder.seats;
+        this.length = builder.length;
+        this.flies = builder.flies;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public boolean isFlies() {
+        return flies;
+    }
+
+    public static class PlaneBuilder {
+        private String mark;
+        private String name;
+        private int seats;
+        private double length;
+        private boolean flies;
+
+        public PlaneBuilder setMark(String mark) {
+            this.mark = mark;
+            return this;
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setSeats(int seats) {
+            this.seats = seats;
+            return this;
+        }
+
+        public PlaneBuilder setLength(double length) {
+            this.length = length;
+            return this;
+        }
+
+        public PlaneBuilder setFlies(boolean flies) {
+            this.flies = flies;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
