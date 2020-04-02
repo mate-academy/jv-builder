56b1f22d01f493967d976e851bb0292a6da63edc
Sumned <vladar91@gmail.com>
2020-04-02 19:18:51 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,75 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String model;
+    private String type;
+    private int volume;
+    private int dateOf;
+    private char series;
+
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.type = builder.type;
+        this.volume = builder.volume;
+        this.dateOf = builder.dateOf;
+        this.series = builder.series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int getDateOf() {
+        return dateOf;
+    }
+
+    public char getSeries() {
+        return series;
+    }
+
+    public class PlaneBuilder {
+
+        private String model;
+        private String type;
+        private int volume;
+        private int dateOf;
+        private char series;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setVolume(int volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        public PlaneBuilder setDateOf(int dateOf) {
+            this.dateOf = dateOf;
+            return this;
+        }
+
+        public PlaneBuilder setSeries(char series) {
+            this.series = series;
+            return this;
+        }
+
+        public Plane build() {
+            return Plane.this;
+        }
+    }
 }
