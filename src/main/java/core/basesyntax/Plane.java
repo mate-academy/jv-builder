4dcc9244df4b36fb4c039bf1c5fa62c2565241a2
Popovych Yurii <popovychyb@gmail.com>
2020-04-02 18:24:57 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,74 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String brand;
+    private String model;
+    private String type;
+    private int range;
+    private int capacity;
 
+    private Plane(PlaneBuilder planeBuilder) {
+        this.brand = planeBuilder.brand;
+        this.model = planeBuilder.model;
+        this.type = planeBuilder.type;
+        this.range = planeBuilder.range;
+        this.capacity = planeBuilder.capacity;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public static class PlaneBuilder {
+        private String brand;
+        private String model;
+        private String type;
+        private int range;
+        private int capacity;
+
+        public PlaneBuilder setBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setRange(int range) {
+            this.range = range;
+            return this;
+        }
+
+        public PlaneBuilder setCapacity(int capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
