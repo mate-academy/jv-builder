35a9759ec5e8ac6bb06b9613027458cd2efd8cea
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-04-02 17:23:43 +0300

    added PlaneBuilder

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,11 +1,77 @@
 package core.basesyntax;
 
-/**
- * <p>Дано класс Plane. Написать класс PlaneBuilder, который реализует паттерн Builder.
- * Добавьте не менее пяти полей в класс Plain. Внести все необходимые изминения в класс Plane
- * которые могут потребоваться для реализации паттерна Builder.</p>
- */
-
 public class Plane {
 
+    private int id;
+    private int seats;
+    private String name;
+    private String model;
+    private String color;
+
+    private Plane(PlaneBuilder builder) {
+        this.id = builder.id;
+        this.seats = builder.seats;
+        this.name = builder.name;
+        this.model = builder.model;
+        this.color = builder.color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public static class PlaneBuilder {
+
+        private int id;
+        private int seats;
+        private String name;
+        private String model;
+        private String color;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public PlaneBuilder setSeats(int seats) {
+            this.seats = seats;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+
+    }
 }
