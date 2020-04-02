6df6ce523428315fbb60489a076b3bf08653dd43
ihor <krohmal.igor777@gmail.com>
2020-04-02 16:21:41 +0300

    implementation class Plane with class PlaneBuilder in him

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,74 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String name;
+    private int size;
+    private String color;
+    private int enginePower;
+    private boolean passenger;
+
+    private Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.name;
+        this.size = planeBuilder.size;
+        this.color = planeBuilder.color;
+        this.enginePower = planeBuilder.enginePower;
+        this.passenger = planeBuilder.passenger;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public boolean isPassenger() {
+        return passenger;
+    }
+
+    public static class PlaneBuilder {
+
+        private String name;
+        private int size;
+        private String color;
+        private int enginePower;
+        private boolean passenger;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setSize(int size) {
+            this.size = size;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setEnginePower(int enginePower) {
+            this.enginePower = enginePower;
+            return this;
+        }
+
+        public void setPassenger(boolean passenger) {
+            this.passenger = passenger;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
