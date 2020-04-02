4c652c0c5bfe5f399ee3a39712c60762d4efa457
Filipp <filipp888@gmail.com>
2020-04-02 12:31:46 +0200

    Added solution for Task 5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,91 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String color;
+    private int speed;
+    private int capacity;
+    private int maxHeight;
 
+    public Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.name;
+        this.color = planeBuilder.color;
+        this.speed = planeBuilder.speed;
+        this.capacity = planeBuilder.capacity;
+        this.maxHeight = planeBuilder.maxHeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{"
+                + "name='" + name
+                + '\''
+                + ", color='" + color
+                + '\''
+                + ", speed=" + speed
+                + ", capacity=" + capacity
+                + ", maxHeight=" + maxHeight
+                + '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getMaxHeight() {
+        return maxHeight;
+    }
+
+    public class PlaneBuilder {
+        private String name;
+        private String color;
+        private int speed;
+        private int capacity;
+        private int maxHeight;
+
+        public PlaneBuilder(String name) {
+            this.name = name;
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setCapacity(int capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        public PlaneBuilder setMaxHeight(int maxHeight) {
+            this.maxHeight = maxHeight;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
