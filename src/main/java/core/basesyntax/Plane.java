7affecb909a44dff6b7c2d1069eadc494cff595b
Yaroslav-Serhii <u.vusochanskiy@gmail.com>
2020-04-02 14:39:42 +0300

    added PlaneBuilder own fields and changed PlaneBuilder setters.

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -19,36 +19,42 @@ public class Plane {
 
     public class PlaneBuilder {
 
+        private int size1;
+        private String model1;
+        private String number1;
+        private long price1;
+        private int age1;
+
         private PlaneBuilder() {
 
         }
 
         public PlaneBuilder setsize(int size) {
-            Plane.this.size = size;
+            this.size1 = size;
 
             return this;
         }
 
         public PlaneBuilder setmodel(String model) {
-            Plane.this.model = model;
+            this.model1 = model;
 
             return this;
         }
 
         public PlaneBuilder setnumber(String number) {
-            Plane.this.number = number;
+            this.number1 = number;
 
             return this;
         }
 
         public PlaneBuilder setage(int age) {
-            Plane.this.age = age;
+            this.age1 = age;
 
             return this;
         }
 
         public PlaneBuilder setprice(long price) {
-            Plane.this.price = price;
+            this.price1 = price;
 
             return this;
         }
