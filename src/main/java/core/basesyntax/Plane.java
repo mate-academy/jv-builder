99d9e01cdc30f6f2d676624cbe4b3791345430b7
Maksym Vakuliuk <vakuliukmaksym@mail.com>
2020-04-02 15:54:09 +0300

    implement class Plane with Builder pattern

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,75 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String mark;
+    private String model;
+    private String typeOfPlane;
+    private int numberOfEngine;
+    private int maxSpeed;
 
+    private Plane(PlaneBuilder planeBuilder) {
+        this.mark = planeBuilder.mark;
+        this.model = planeBuilder.model;
+        this.typeOfPlane = planeBuilder.typeOfPlane;
+        this.numberOfEngine = planeBuilder.numberOfEngine;
+        this.maxSpeed = planeBuilder.maxSpeed;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getTypeOfPlane() {
+        return typeOfPlane;
+    }
+
+    public int getNumberOfEngine() {
+        return numberOfEngine;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public static class PlaneBuilder {
+        private String mark;
+        private String model;
+        private String typeOfPlane;
+        private int numberOfEngine;
+        private int maxSpeed;
+
+        public PlaneBuilder setMark(String mark) {
+            this.mark = mark;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setTypeOfPlane(String typeOfPlane) {
+            this.typeOfPlane = typeOfPlane;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfEngine(int numberOfEngine) {
+            this.numberOfEngine = numberOfEngine;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+
+    }
 }
