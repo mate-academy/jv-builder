e05c21f596f2207b6890b5bce1ddc9bdbbc118ca
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-04-02 16:42:26 +0300

    added solution for homework 5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,75 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private final int seatsCount;
+    private final int maxFlightDistance;
+    private final String manufacturer;
+    private final String model;
+    private final String airline;
 
+    private Plane(PlaneBuilder pb) {
+        this.seatsCount = pb.seatsCount;
+        this.maxFlightDistance = pb.maxFlightDistance;
+        this.manufacturer = pb.manufacturer;
+        this.model = pb.model;
+        this.airline = pb.airline;
+    }
+
+    public int getSeatsCount() {
+        return seatsCount;
+    }
+
+    public int getMaxFlightDistance() {
+        return maxFlightDistance;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public static class PlaneBuilder {
+
+        private int seatsCount;
+        private int maxFlightDistance;
+        private String manufacturer;
+        private String model;
+        private String airline;
+
+        public PlaneBuilder setSeatsCount(int seatsCount) {
+            this.seatsCount = seatsCount;
+            return this;
+        }
+
+        public PlaneBuilder setMaxFlightDistance(int maxFlightDistance) {
+            this.maxFlightDistance = maxFlightDistance;
+            return this;
+        }
+
+        public PlaneBuilder setManufacturer(String manufacturer) {
+            this.manufacturer = manufacturer;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setAirline(String airline) {
+            this.airline = airline;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
