2bfaed37710fe51689aa5cc313173ca07225a538
interami <interami@ukr.net>
2020-04-02 15:13:56 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private double weight;
+    private int price;
+    private int length;
+    private String type;
 
+    private Plane(PlaneBuilder planeBuilder) {
+        this.length = planeBuilder.length;
+        this.name = planeBuilder.name;
+        this.price = planeBuilder.price;
+        this.type = planeBuilder.type;
+        this.weight = planeBuilder.weight;
+    }
+
+    public class PlaneBuilder {
+        private String name;
+        private double weight;
+        private int price;
+        private int length;
+        private String type;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setPrice(int price) {
+            this.price = price;
+            return this;
+        }
+
+        public PlaneBuilder setLength(int length) {
+            this.length = length;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
