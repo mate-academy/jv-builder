bd5fdaeb9f3ebd92dfa48fbf6c54378fcfdf7900
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-04-02 14:40:23 +0300

    hw_5_2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,77 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private final String name;
+    private final String title;
+    private final int takeoffWeight;
+    private final int engine;
+    private final int numberOfSeats;
 
+    private Plane(PlaneBuilder newPlan) {
+        this.name = newPlan.name;
+        this.title = newPlan.title;
+        this.takeoffWeight = newPlan.takeoffWeight;
+        this.engine = newPlan.engine;
+        this.numberOfSeats = newPlan.numberOfSeats;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getTakeoffWeight() {
+        return takeoffWeight;
+    }
+
+    public int getEngine() {
+        return engine;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String title;
+        private int takeoffWeight;
+        private int engine;
+        private int numberOfSeats;
+
+        public PlaneBuilder() {
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public PlaneBuilder setTakeoffWeight(int takeoffWeight) {
+            this.takeoffWeight = takeoffWeight;
+            return this;
+        }
+
+        public PlaneBuilder setEngine(int engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfSeats(int numberOfSeats) {
+            this.numberOfSeats = numberOfSeats;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(PlaneBuilder.this);
+        }
+    }
 }
