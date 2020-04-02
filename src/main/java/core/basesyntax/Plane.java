572d3b49ce345ac44b9128d551e69a91842c1753
SpringWay <44027294+SpringWay@users.noreply.github.com>
2020-04-02 19:59:27 +0300

    added solution to hw-5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String color;
+    private int speed;
+    private int wing;
+    private int engineVolume;
+    private int length;
 
+    private Plane(PlaneBuilder builder) {
+        this.color = builder.color;
+        this.speed = builder.speed;
+        this.wing = builder.wing;
+        this.engineVolume = builder.engineVolume;
+        this.length = builder.length;
+    }
+
+    public static class PlaneBuilder {
+        private String color;
+        private int speed;
+        private int wing;
+        private int engineVolume;
+        private int length;
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setWing(int wing) {
+            this.wing = wing;
+            return this;
+        }
+
+        public PlaneBuilder setEngineVolume(int engineVolume) {
+            this.engineVolume = engineVolume;
+            return this;
+        }
+
+        public void setLength(int length) {
+            this.length = length;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
+
