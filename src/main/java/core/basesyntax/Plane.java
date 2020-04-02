12060ac6dbc95eb79b8ce0167ded3f16c3781524
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2020-04-03 00:02:58 +0300

    Added solution hw-5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String model;
+    private String type;
+    private String engineType;
+    private int grt;
+    private int pwr;
 
+    public Plane(PlaneBuilder builder) {
+        this.model = model;
+        this.type = type;
+        this.engineType = engineType;
+        this.grt = grt;
+        this.pwr = pwr;
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private String type;
+        private String engineType;
+        private int grt;
+        private int pwr;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setEngineType(String engineType) {
+            this.engineType = engineType;
+            return this;
+        }
+
+        public PlaneBuilder setGrt(int grt) {
+            this.grt = grt;
+            return this;
+        }
+
+        public PlaneBuilder setPwr(int pwr) {
+            this.pwr = pwr;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
