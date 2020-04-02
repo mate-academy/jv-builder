1342ccb0e6b4a4c41f2d581326cc5d1ecdec4c53
NikolayAbysov <nikolay.abysov@ukr.net>
2020-04-02 19:00:09 +0300

    Main class removed

--- src/main/java/core/basesyntax/Main.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Plane.PlaneBuilder pb = new Plane.PlaneBuilder("MIG");
-        Plane plane = pb.build();
-    }
-}
