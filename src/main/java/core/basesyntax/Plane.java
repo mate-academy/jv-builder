d2ae0065116df9ce28dfc7a24c2309fa7e4b14e8
ana-dav <opa.super.op@gmail.com>
2020-04-02 21:56:57 +0300

    5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,11 +1,64 @@
 package core.basesyntax;
 
-/**
- * <p>Дано класс Plane. Написать класс PlaneBuilder, который реализует паттерн Builder.
- * Добавьте не менее пяти полей в класс Plain. Внести все необходимые изминения в класс Plane
- * которые могут потребоваться для реализации паттерна Builder.</p>
- */
-
 public class Plane {
 
+    private String brand;
+    private int model;
+    private int power;
+    private String color;
+    private int wheelsDiameter;
+
+    public Plane(PlaneBuilder planeBuilder) {
+        this.brand = planeBuilder.brand;
+        this.model = planeBuilder.model;
+        this.power = planeBuilder.power;
+        this.color = planeBuilder.color;
+        this.wheelsDiameter = planeBuilder.wheelsDiameter;
+    }
+
+    public static class PlaneBuilder {
+        //Required
+        private String brand;
+        private int model;
+        //Optional
+        private int power = 0;
+        private String color = null;
+        private int wheelsDiameter = 0;
+
+        public PlaneBuilder(String brand, int model) {
+            this.brand = brand;
+            this.model = model;
+        }
+
+        public PlaneBuilder setBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public PlaneBuilder setModel(int model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setPower(int power) {
+            this.power = power;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setWheelsDiameter(int wheelsDiameter) {
+            this.wheelsDiameter = wheelsDiameter;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
+
+
