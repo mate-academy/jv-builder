5ff79e0df1bf5d7ba55381ef1f993f3421cfce49
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-04-02 16:41:38 +0300

    added realizetion of Build patter

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,74 @@ package core.basesyntax;
 
 public class Plane {
 
+    private Integer power;
+    private Integer size;
+    private String color;
+    private Integer seats;
+    private String model;
+
+    public Plane(PlaneBuilder builderForPlane) {
+        this.model = builderForPlane.model;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Integer getSeats() {
+        return seats;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public class PlaneBuilder {
+        private Integer power;
+        private Integer size;
+        private String color;
+        private Integer seats;
+        private String model;
+
+        private PlaneBuilder() {
+        }
+
+        public PlaneBuilder setPower(Integer power) {
+            Plane.this.power = power;
+            return this;
+        }
+
+        public PlaneBuilder setSize(Integer size) {
+            Plane.this.size = size;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            Plane.this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setSeats(Integer seats) {
+            Plane.this.seats = seats;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            Plane.this.model = model;
+            return this;
+        }
+
+        public Plane build() {
+            return Plane.this;
+        }
+
+    }
 }
