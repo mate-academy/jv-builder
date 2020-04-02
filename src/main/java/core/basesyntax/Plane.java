b31afb4c39851b5e82f9592f83c9b47599bbe43d
Tonio-An <inncocentguy@gmail.com>
2020-04-02 17:55:39 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,48 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String brand;
+    private String model;
+    private int humanCapacity;
+    private int maxSpeed;
+    private int engineAmount;
+
+    private Plane(PlaneBuilder planeBuilder) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public class PlaneBuilder {
+        public PlaneBuilder setBrand(String brand) {
+            Plane.this.brand = brand;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            Plane.this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setHumanCapacity(int humanCapacity) {
+            Plane.this.humanCapacity = humanCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            Plane.this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setEngineAmount(int engineAmount) {
+            Plane.this.engineAmount = engineAmount;
+            return this;
+        }
+
+        public PlaneBuilder build() {
+            return PlaneBuilder.this;
+        }
+    }
 }
+
