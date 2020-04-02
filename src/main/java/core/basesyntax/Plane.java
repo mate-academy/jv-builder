a4032263182bb99817a339d4aa82360e671e24ac
serg-ksv <serg.ksv1@gmail.com>
2020-04-02 15:20:02 +0300

    Added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,62 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String type;
+    private String typeEngine;
+    private int range;
+    private int maxSpeed;
+    private int numberOfEngines;
+    private int capacity;
 
+    public Plane(PlaneBuilder builder) {
+        this.type = builder.type;
+        this.typeEngine = builder.typeEngine;
+        this.range = builder.range;
+        this.maxSpeed = builder.maxSpeed;
+        this.numberOfEngines = builder.numberOfEngines;
+        this.capacity = builder.capacity;
+    }
+
+    public static class PlaneBuilder {
+        private String type;
+        private String typeEngine;
+        private int range;
+        private int maxSpeed;
+        private int numberOfEngines;
+        private int capacity;
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setTypeEngine(String typeEngine) {
+            this.typeEngine = typeEngine;
+            return this;
+        }
+
+        public PlaneBuilder setRange(int range) {
+            this.range = range;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfEngines(int numberOfEngines) {
+            this.numberOfEngines = numberOfEngines;
+            return this;
+        }
+
+        public PlaneBuilder setCapacity(int capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
