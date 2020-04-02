ab03c0fde23706834c9880b9d839f735886e159a
unknown <bavikaalexey@gmail.com>
2020-04-03 00:01:04 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,79 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String type;
+    private String country;
+    private int passengerSeats;
+    private int speed;
 
+    public Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.name;
+        this.type = planeBuilder.type;
+        this.country = planeBuilder.country;
+        this.passengerSeats = planeBuilder.passengerSeats;
+        this.speed = planeBuilder.speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getPassengerSeats() {
+        return passengerSeats;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String type;
+        private String country;
+        private int passengerSeats;
+        private int speed;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+
+            return this;
+        }
+
+        public PlaneBuilder setCountry(String country) {
+            this.country = country;
+
+            return this;
+        }
+
+        public PlaneBuilder setPassengerSeats(int passengerSeats) {
+            this.passengerSeats = passengerSeats;
+
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(int speed) {
+            this.speed = speed;
+
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
