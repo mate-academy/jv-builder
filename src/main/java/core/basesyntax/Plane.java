d6719c7378543a3c1e560e913f37cb56234fbc1e
Ludchenko <skillet367@icloud.com>
2020-04-03 13:23:50 +0300

    fixed empty lines and improved constructor

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -27,31 +27,29 @@ public class Plane {
 
         public PlaneBuilder setPlaneModel(String planeModel) {
             this.planeModel = planeModel;
-
+            this.planeEngineType = planeEngineType;
+            this.planeSize = planeSize;
+            this.planeSpeed = planeSpeed;
             return this;
         }
 
         public PlaneBuilder setPlaneEngineType(String planeEngineType) {
             this.planeEngineType = planeEngineType;
-
             return this;
         }
 
         public PlaneBuilder setPlaneSize(int planeSize) {
             this.planeSize = planeSize;
-
             return this;
         }
 
         public PlaneBuilder setPlaneSpeed(int planeSpeed) {
             this.planeSpeed = planeSpeed;
-
             return this;
         }
 
         public PlaneBuilder setPlaneColor(String planeColor) {
             this.planeColor = planeColor;
-
             return this;
         }
 
