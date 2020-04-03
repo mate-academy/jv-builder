61ff36c782ea8abcc31ac79b8a3409c8136e2015
My <vasylnv19@gmail.com>
2020-04-03 14:00:03 +0300

    Solution_HW_5_2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,90 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private int power;
+    private double capacityEngine;
+    private int numberOfSeats;
+    private String type;
 
+    public Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.getName();
+        this.power = planeBuilder.getPower();
+        this.capacityEngine = planeBuilder.getCapacityEngine();
+        this.numberOfSeats = planeBuilder.getNumberOfSeats();
+        this.type = planeBuilder.getType();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public double getCapacityEngine() {
+        return capacityEngine;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private int power;
+        private double capacityEngine;
+        private int numberOfSeats;
+        private String type;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getPower() {
+            return power;
+        }
+
+        public void setPower(int power) {
+            this.power = power;
+        }
+
+        public double getCapacityEngine() {
+            return capacityEngine;
+        }
+
+        public void setCapacityEngine(double capacityEngine) {
+            this.capacityEngine = capacityEngine;
+        }
+
+        public int getNumberOfSeats() {
+            return numberOfSeats;
+        }
+
+        public void setNumberOfSeats(int numberOfSeats) {
+            this.numberOfSeats = numberOfSeats;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
