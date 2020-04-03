ab59ee7b9374105c4704316def77cce202d6b396
VovkAA <vovkandrew@gmail.com>
2020-04-03 13:23:13 +0300

    Done

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,76 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String brand;
+    private String model;
+    private int maxSpeed;
+    private int maxHeight;
+    private double engineVolume;
+
+    private Plane(PlaneBuilder builder) {
+        this.brand = builder.brand;
+        this.model = builder.model;
+        this.maxSpeed = builder.maxSpeed;
+        this.maxHeight = builder.maxHeight;
+        this.engineVolume = builder.engineVolume;;
+    }
+
+    public static class PlaneBuilder {
+
+        private String brand;
+        private String model;
+        private int maxSpeed;
+        private int maxHeight;
+        private double engineVolume;
+
+        public PlaneBuilder setBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setMaxHeight(int maxHeight) {
+            this.maxHeight = maxHeight;
+            return this;
+        }
+
+        public PlaneBuilder setEngineVolume(double engineVolume) {
+            this.engineVolume = engineVolume;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getMaxHeight() {
+        return maxHeight;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
 }
