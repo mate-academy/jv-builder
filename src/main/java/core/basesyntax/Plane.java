c306c2bfbe9eccd8be850816f9c0e16bac026fd2
Artem Vremere <vremere_a@ukr.net>
2020-07-31 16:47:51 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,79 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String name;
+    private String type;
+    private String color;
+    private double power;
+    private double speed;
+
+    public Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.name;
+        this.type = planeBuilder.type;
+        this.color = planeBuilder.color;
+        this.power = planeBuilder.power;
+        this.speed = planeBuilder.speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public static class PlaneBuilder {
+
+        private String name;
+        private String type;
+        private String color;
+        private double power;
+        private double speed;
+
+        public PlaneBuilder(String name) {
+            this.name = name;
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setPower(double power) {
+            this.power = power;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(double speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
