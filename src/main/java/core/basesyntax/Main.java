b460decdd9e441584f8349fbd712bfb15119f182
MaliukDaria <maliuk.dasha@gmail.com>
2020-07-31 15:44:20 +0300

    added Main class

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        Plane.PlaneBuilder planeBuilder = new Plane.PlaneBuilder();
+        Plane myPlane = planeBuilder.setId(1)
+                .setModel("Бройлер 747")
+                .setNumberOfPassenger(13)
+                .setIsSeaplane(true)
+                .setColour("Pink")
+                .build();
+        System.out.println(myPlane);
+    }
+}
