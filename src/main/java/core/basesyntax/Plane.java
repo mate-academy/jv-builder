a12c0c532f363b755e01bfa68f4c3aa2017848fb
Leonid <rommel@ukr.net>
2020-07-31 21:21:07 +0300

    task done

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,74 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String brand;
+    private String model;
+    private int capacity;
+    private int maxSpeed;
+    private int rangeOfFlight;
 
+    public Plane(PlaneBuilder planeBuilder) {
+        brand = planeBuilder.brand;
+        model = planeBuilder.model;
+        capacity = planeBuilder.capacity;
+        maxSpeed = planeBuilder.maxSpeed;
+        rangeOfFlight = planeBuilder.rangeOfFlight;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getRangeOfFlight() {
+        return rangeOfFlight;
+    }
+
+    public static class PlaneBuilder {
+        private String brand;
+        private String model;
+        private int capacity;
+        private int maxSpeed;
+        private int rangeOfFlight;
+
+        public PlaneBuilder setBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setCapacity(int capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setRangeOfFlight(int rangeOfFlight) {
+            this.rangeOfFlight = rangeOfFlight;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
