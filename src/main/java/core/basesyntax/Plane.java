2142b713c0db13eb056a9f04e7d1ce0b8dae3a62
Alexey <gradiviro@gmail.com>
2020-07-31 12:52:11 +0300

    5.2 task solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,76 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int planeID;
+    private String planeName;
+    private boolean isInAir;
+    private String pilotName;
+    private int gasCapacity;
 
+    private Plane(PlaneBuilder builder) {
+        this.planeName = builder.setPlaneName;
+    }
+
+    public int getPlaneID() {
+        return planeID;
+    }
+
+    public String getPlaneName() {
+        return planeName;
+    }
+
+    public boolean getIsInAir() {
+        return isInAir;
+    }
+
+    public String getPilotname() {
+        return pilotName;
+    }
+
+    public int getGasCapacity() {
+        return gasCapacity;
+    }
+
+    public class PlaneBuilder {
+        public String setPlaneName;
+
+        private PlaneBuilder() {
+
+        }
+
+        public PlaneBuilder setPlaneID(int planeID) {
+            Plane.this.planeID = planeID;
+
+            return this;
+        }
+
+        public PlaneBuilder setPlaneName(String planeName) {
+            Plane.this.planeName = planeName;
+
+            return this;
+        }
+
+        public PlaneBuilder setIsInAir(boolean isInAir) {
+            Plane.this.isInAir = isInAir;
+
+            return this;
+        }
+
+        public PlaneBuilder setPilotName(String pilotName) {
+            Plane.this.pilotName = pilotName;
+
+            return this;
+        }
+
+        public PlaneBuilder setGasCapacity(int gasCapacity) {
+            Plane.this.gasCapacity = gasCapacity;
+
+            return this;
+        }
+
+        public Plane build() {
+
+            return Plane.this;
+        }
+    }
 }
