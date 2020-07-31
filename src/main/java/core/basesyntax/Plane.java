06c0620c50dc94f28ba412824ca78a86c5284404
glones <polivanchuk2013@gmail.com>
2020-07-31 16:54:47 +0300

    Problem with test "You should have only one constructor in the Plane.class "

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,56 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String model;
+    private String manufacturer;
+    private String engineName;
+    private String constructorName;
+
+    private Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.model = builder.model;
+        this.manufacturer = builder.manufacturer;
+        this.engineName = builder.engineName;
+        this.constructorName = builder.constructorName;
+
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String model;
+        private String manufacturer;
+        private String engineName;
+        private String constructorName;
+
+        public PlaneBuilder setName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model){
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setManufacturer(String manufacturer){
+            this.manufacturer = manufacturer;
+            return this;
+        }
+
+        public PlaneBuilder setEngineName(String engineName){
+            this.engineName = engineName;
+            return this;
+        }
+
+        public PlaneBuilder setConstructorName(String constructorName){
+            this.constructorName = constructorName;
+            return this;
+        }
+
+        public Plane build(){
+            return new Plane(this);
+        }
+    }
 
 }
