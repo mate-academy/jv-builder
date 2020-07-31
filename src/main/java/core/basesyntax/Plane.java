457edd3d591f7705e08c0cfe99980b38b1791dfb
TimoxaV <vadim2493@gmail.com>
2020-07-31 14:55:01 +0300

    added solution 5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,63 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String color;
+    private int weight;
+    private int passengerNumber;
+    private int petrolVolume;
 
+    private Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.color = builder.color;
+        this.weight = builder.weight;
+        this.passengerNumber = builder.passengerNumber;
+        this.petrolVolume = builder.petrolVolume;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String color;
+        private int weight;
+        private int passengerNumber;
+        private int petrolVolume;
+
+        public PlaneBuilder(String name, String color, int weight,
+                            int passengerNumber, int petrolVolume) {
+            this.name = name;
+            this.color = color;
+            this.weight = weight;
+            this.passengerNumber = passengerNumber;
+            this.petrolVolume = petrolVolume;
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setPassengerNumber(int passengerNumber) {
+            this.passengerNumber = passengerNumber;
+            return this;
+        }
+
+        public PlaneBuilder setPetrolVolume(int petrolVolume) {
+            this.petrolVolume = petrolVolume;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
