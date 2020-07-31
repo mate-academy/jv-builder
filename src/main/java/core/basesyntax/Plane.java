146d579d3fa7a54d9325478b97fe40770a8463b8
tarkaiev <tarkaievd@gmail.com>
2020-07-31 20:32:39 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,80 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String manufacturer;
+    private String model;
+    private int boardNumber;
+    private int passengerCapacity;
+    private int yearOfManufacturing;
 
+    private Plane(PlaneBuilder builder) {
+        this.manufacturer = builder.manufacturer;
+        this.model = builder.model;
+        this.boardNumber = builder.boardNumber;
+        this.passengerCapacity = builder.passengerCapacity;
+        this.yearOfManufacturing = builder.yearOfManufacturing;
+
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getBoardNumber() {
+        return boardNumber;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public int getYearOfManufacturing() {
+        return yearOfManufacturing;
+    }
+
+    public static class PlaneBuilder {
+        private String manufacturer;
+        private String model;
+        private int boardNumber;
+        private int passengerCapacity;
+        private int yearOfManufacturing;
+
+        public PlaneBuilder setManufacturer(String manufacturer) {
+            this.manufacturer = manufacturer;
+
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+
+            return this;
+        }
+
+        public PlaneBuilder setBoardNumber(int boardNumber) {
+            this.boardNumber = boardNumber;
+
+            return this;
+        }
+
+        public PlaneBuilder setPassengerCapacity(int passengerCapacity) {
+            this.passengerCapacity = passengerCapacity;
+
+            return this;
+        }
+
+        public PlaneBuilder setYearOfManufacturing(int yearOfManufacturing) {
+            this.yearOfManufacturing = yearOfManufacturing;
+
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
