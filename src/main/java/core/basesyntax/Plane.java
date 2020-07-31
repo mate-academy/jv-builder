6c4c672b4ba117222a32777cdf801f7a7edd1ba2
jlightgreen <ShvabovichJulia@gmail.com>
2020-07-31 14:30:10 +0300

    hw-5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,50 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int numberOfSeats;
+    private int maxSpeed;
+    private long maxTakeoffWeight;
+    private long maxFlightAltitude;
+    private String countryOfProduction;
 
+    private Plane(PlaneBuilder planeBuilder) {
+    }
+
+    public class PlaneBuilder {
+        private Plane plane;
+
+        private PlaneBuilder() {
+            plane = new Plane(this);
+        }
+
+        public PlaneBuilder setNumberOfSeats(int numberOfSeats) {
+            plane.numberOfSeats = numberOfSeats;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            plane.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setMaxTakeoffWeight(long maxTakeoffWeight) {
+            plane.maxTakeoffWeight = maxTakeoffWeight;
+            return this;
+        }
+
+        public PlaneBuilder setMaxFlightAltitude(long maxFlightAltitude) {
+            plane.maxFlightAltitude = maxFlightAltitude;
+            return this;
+        }
+
+        public PlaneBuilder setCountryOfProduction(String countryOfProduction) {
+            plane.countryOfProduction = countryOfProduction;
+            return this;
+        }
+
+        public Plane build() {
+            return plane;
+        }
+
+    }
 }
