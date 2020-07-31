3d2803cfc9ec80fbff6a65c0b7669888e2fca3ac
Ilya.Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2020-07-31 15:20:52 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,55 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String mark;
+    private String model;
+    private double speed;
+    private double weight;
+    private int numberOfTurbines;
+
+    public Plane(PlaneBuilder planeBuilder) {
+        this.mark = planeBuilder.mark;
+        this.model = planeBuilder.model;
+        this.speed = planeBuilder.speed;
+        this.weight = planeBuilder.weight;
+        this.numberOfTurbines = planeBuilder.numberOfTurbines;
+    }
+
+    public static class PlaneBuilder {
+
+        private String mark;
+        private String model;
+        private double speed;
+        private double weight;
+        private int numberOfTurbines;
+
+        public PlaneBuilder setMark(String mark) {
+            this.mark = mark;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(double speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(double weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfTurbines(int numberOfTurbines) {
+            this.numberOfTurbines = numberOfTurbines;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
