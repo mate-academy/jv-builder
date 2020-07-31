1a12e2d7957456b998f04156304ffbfd5d477840
Roman <matviichuk.office@gmail.com>
2020-07-31 22:41:31 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String color;
+    private String brand;
+    private String airLines;
+    private int speed;
+    private int yearOfIssue;
 
+    public static class PlaneBuilder {
+        private String color;
+        private String brand;
+        private String airLines;
+        private int speed;
+        private int yearOfIssue;
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public PlaneBuilder setAirLines(String airLines) {
+            this.airLines = airLines;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setYearOfIssue(int yearOfIssue) {
+            this.yearOfIssue = yearOfIssue;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
+
+    private Plane(PlaneBuilder builder) {
+        this.color = color;
+        this.brand = brand;
+        this.airLines = airLines;
+        this.speed = speed;
+        this.yearOfIssue = yearOfIssue;
+    }
 }
