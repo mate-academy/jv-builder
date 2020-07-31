8cd4df32bf251dfcd9cb9c5cc9f932dcedfac3a8
MaliukDaria <maliuk.dasha@gmail.com>
2020-07-31 17:28:18 +0300

    changed language to english

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Plane.PlaneBuilder planeBuilder = new Plane.PlaneBuilder();
         Plane myPlane = planeBuilder.setId(1)
-                .setModel("Бройлер 747")
+                .setModel("Broiler 747")
                 .setNumberOfPassenger(13)
                 .setIsSeaplane(true)
                 .setColour("Pink")
