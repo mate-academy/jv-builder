6a2bcda814aae93e44ee60f65a2aa17ddaa58fb2
ROMASHSX <andriiromash@gmail.com>
2020-07-31 13:45:28 +0300

    Task 2 is done

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,74 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String modelName;
+    private String type;
+    private Double planeLength;
+    private String countryOfProducing;
+    private boolean isNewestModel;
 
+    private Plane(PlaneBuilder plainBuilder) {
+        this.modelName = plainBuilder.modelName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Double getPlaneLength() {
+        return planeLength;
+    }
+
+    public String getCountryOfProducing() {
+        return countryOfProducing;
+    }
+
+    public boolean isNewestModel() {
+        return isNewestModel;
+    }
+
+    public static class PlaneBuilder {
+        private String modelName;
+        private String type;
+        private Double planeLength;
+        private String countryOfProducing;
+        private boolean isNewestModel;
+
+        private PlaneBuilder(String modelName) {
+            this.modelName = modelName;
+        }
+
+        public PlaneBuilder setModelName(String modelName) {
+            this.modelName = modelName;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setPlaneLength(double planeLength) {
+            this.planeLength = planeLength;
+            return this;
+        }
+
+        public PlaneBuilder setCountryOfProducing(String countryOfProducing) {
+            this.countryOfProducing = countryOfProducing;
+            return this;
+        }
+
+        public PlaneBuilder setIsNewestModel(boolean isNewestModel) {
+            this.isNewestModel = isNewestModel;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
