1af58de6234d3473591faaf20b9f07a45b1401df
Alkey <Sirj777sirj@gmail.com>
2020-07-31 14:12:28 +0300

    homework 5.2

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,55 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String name;
+    private int maxSpeed;
+    private int maxHeight;
+    private int numberOfPassengers;
+    private String manufacturer;
+
+    public Plane(PlaneBuilder builder) {
+
+    }
+
+    public static class PlaneBuilder {
+
+        private String name;
+        private int maxSpeed;
+        private int maxHeight;
+        private int numberOfPassengers;
+        private String manufacturer;
+
+        public PlaneBuilder(String name, int maxSpeed, int maxHeight,
+                            int numberOfPassengers, String manufacturer) {
+            this.name = name;
+            this.maxSpeed = maxSpeed;
+            this.maxHeight = maxHeight;
+            this.numberOfPassengers = numberOfPassengers;
+            this.manufacturer = manufacturer;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+        }
+
+        public void setMaxHeight(int maxHeight) {
+            this.maxHeight = maxHeight;
+        }
+
+        public void setNumberOfPassengers(int numberOfPassengers) {
+            this.numberOfPassengers = numberOfPassengers;
+        }
+
+        public void setManufacturer(String manufacturer) {
+            this.manufacturer = manufacturer;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
