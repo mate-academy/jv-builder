8d0a19a85a22de2724a0f7e38e9b25eba5ecde61
Your Name <i.dymytreyeva@gmail.com>
2020-07-31 14:36:30 +0300

    solution added

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,70 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private int maxSpeed;
+    private String model;
+    private int weight;
+    private String type;
+    private int amountOfPassengers;
 
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.setModel;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getAmountOfPassengers() {
+        return amountOfPassengers;
+    }
+
+    public class PlaneBuilder {
+        public String setModel;
+
+        private PlaneBuilder() {
+
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            Plane.this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            Plane.this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            Plane.this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            Plane.this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setAmountOfPassengers(int amountOfPassengers) {
+            Plane.this.amountOfPassengers = amountOfPassengers;
+            return this;
+        }
+
+        public Plane build() {
+            return Plane.this;
+        }
+    }
 }
