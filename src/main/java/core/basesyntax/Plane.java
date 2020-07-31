d4f1fae860aa420f260784c0f91963631837b4c0
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-07-31 14:33:41 +0300

    solution added

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,11 +1,54 @@
 package core.basesyntax;
 
-/**
- * <p>Дано класс Plane. Написать класс PlaneBuilder, который реализует паттерн Builder.
- * Добавьте не менее пяти полей в класс Plain. Внести все необходимые изминения в класс Plane
- * которые могут потребоваться для реализации паттерна Builder.</p>
- */
-
 public class Plane {
+    private String geography;
+    private String color;
+    private int area;
+    private int height;
+    private boolean isInhabited;
 
+    private Plane(PlaneBuilder builder) {
+        this.geography = builder.geography;
+        this.color = builder.color;
+        this.area = builder.area;
+        this.height = builder.height;
+        this.isInhabited = builder.isInhabited;
+    }
+
+    public class PlaneBuilder {
+        private String geography;
+        private String color;
+        private int area;
+        private int height;
+        private boolean isInhabited;
+
+        public PlaneBuilder setGeography(String geography) {
+            this.geography = geography;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setArea(int area) {
+            this.area = area;
+            return this;
+        }
+
+        public PlaneBuilder setHeight(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public PlaneBuilder setisInhabited(boolean isInhabited) {
+            this.isInhabited = isInhabited;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
