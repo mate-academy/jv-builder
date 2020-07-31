a0efc1d54b9b55d89a9a21651b40d2b696ad117f
Laszlo Petechuk <frostpv@gmail.com>
2020-07-31 14:36:56 +0300

    My solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,75 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String model;
+    private String colour;
+    private int speed;
+    private int places;
+    private int weight;
+
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.colour = builder.colour;
+        this.speed = builder.speed;
+        this.places = builder.places;
+        this.weight = builder.weight;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getPlaces() {
+        return places;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public static class PlaneBuilder {
+
+        private String model;
+        private String colour;
+        private int speed;
+        private int places;
+        private int weight;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setColour(String colour) {
+            this.colour = colour;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setPlaces(int places) {
+            this.places = places;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
