c9a16a7ebf017e5fac3177c5ceb3d8d99c605936
Victoria-Moiseienko <teasevic@gmail.com>
2020-08-01 12:23:01 +0300

    solution with passed tests

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        Plane plane = new Plane.PlaneBuilder()
+                .setName("Boeing")
+                .setCapacity(450)
+                .setFlightRange(950)
+                .setPower(1000)
+                .setWingspan(60)
+                .build();
+
+
+    }
+
+}
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
 
+
 /**
  * <p>Дано класс Plane. Написать класс PlaneBuilder, который реализует паттерн Builder.
  * Добавьте не менее пяти полей в класс Plain. Внести все необходимые изминения в класс Plane
@@ -7,5 +8,48 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private int capacity;
+    private int flightRange;
+    private int power;
+    private int wingspan;
 
+
+    public Plane(PlaneBuilder planeBuilder) {
+    }
+
+    public static class PlaneBuilder {
+        private Plane newPlane;
+
+        PlaneBuilder() {}
+
+        public PlaneBuilder setName(String name) {
+            newPlane.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setCapacity(int capacity) {
+            newPlane.capacity = capacity;
+            return this;
+        }
+
+        public PlaneBuilder setFlightRange(int flightRange) {
+            newPlane.flightRange = flightRange;
+            return this;
+        }
+
+        public PlaneBuilder setPower(int power) {
+            newPlane.power = power;
+            return this;
+        }
+
+        public PlaneBuilder setWingspan(int wingspan) {
+            newPlane.wingspan = wingspan;
+            return this;
+        }
+
+        public Plane build() {
+            return newPlane;
+        }
+    }
 }
