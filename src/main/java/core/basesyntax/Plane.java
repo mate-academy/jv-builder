5ff3e40cd2aa47e93c6e17e66679a3b29254c6d1
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-08-01 13:32:08 +0300

    hw

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -8,4 +8,55 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String model;
+    private int wingsNumber;
+    private double wingsLength;
+    private int passengerSeats;
+    private boolean swimAbility;
+    private int pilotsNumber;
+
+    private Plane(PlaneBuilder planeBuilder) {
+
+    }
+
+    public class PlaneBuilder {
+
+        private PlaneBuilder() {
+
+        }
+
+        public PlaneBuilder setModel(String model) {
+            Plane.this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setWingsNumber(int wingsNumber) {
+            Plane.this.wingsNumber = wingsNumber;
+            return this;
+        }
+
+        public PlaneBuilder setWingsLength(double wingsLength) {
+            Plane.this.wingsLength = wingsLength;
+            return this;
+        }
+
+        public PlaneBuilder setPassengerSeats(int passengerSeats) {
+            Plane.this.passengerSeats = passengerSeats;
+            return this;
+        }
+
+        public PlaneBuilder setSwimAbility(boolean swimAbility) {
+            Plane.this.swimAbility = swimAbility;
+            return this;
+        }
+
+        public PlaneBuilder setPilotsNumber(int pilotsNumber) {
+            Plane.this.pilotsNumber = pilotsNumber;
+            return this;
+        }
+
+        public Plane build() {
+            return Plane.this;
+        }
+    }
 }
