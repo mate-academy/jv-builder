95a61df38cb2670bc552aa5ee6c6a875050cf75c
Jah <moskovienkor@gmail.com>
2020-08-02 16:07:50 +0300

    HOMEWORK-5-2: - done;

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,74 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String model;
+    private String color;
+    private int weight;
+    private int maxSpeed;
+    private int passengerCapacity;
 
+    public Plane(PlaneBuilder planeBuilder) {
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    public int getPassengerCapacity() {
+        return this.passengerCapacity;
+    }
+
+    public class PlaneBuilder {
+        private Plane plane;
+
+        private String model;
+        private String color;
+        private int weight;
+        private int maxSpeed;
+        private int passengerCapacity;
+
+        public PlaneBuilder() {
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setPassengerCapacity(int passengerCapacity) {
+            this.passengerCapacity = passengerCapacity;
+            return this;
+        }
+
+        public Plane build() {
+            return Plane.this;
+        }
+    }
 }
