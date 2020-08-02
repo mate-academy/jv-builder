6687e7dd1bfa2b5e152ee676acdaef453c896f24
vladd <vladdyachkin11@gmail.com>
2020-08-02 12:27:53 +0300

    added solution

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,54 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String color;
+    private int weight;
+    private int length;
+    private int width;
 
+    private Plane(PlaneBuilder builder) {
+        this.name = builder.name;
+        this.color = builder.color;
+        this.weight = builder.weight;
+        this.length = builder.length;
+        this.width = builder.width;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String color;
+        private int weight;
+        private int length;
+        private int width;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setLength(int length) {
+            this.length = length;
+            return this;
+        }
+
+        public PlaneBuilder setWidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
