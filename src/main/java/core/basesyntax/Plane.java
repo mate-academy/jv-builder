5478ca02d9f4a174f63f05e6faf01544724f5d2d
vitalka1994 <59143874+vitalka1994@users.noreply.github.com>
2020-08-02 16:50:23 +0300

    HW 55-22

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,56 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String model;
+    private String color;
+    private int speed;
+    private int weight;
+    private String brand;
+
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.color = builder.color;
+        this.speed = builder.speed;
+        this.weight = builder.weight;
+        this.brand = builder.brand;
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private String color;
+        private int speed;
+        private int weight;
+        private String brand;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setSpeed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 
 }
+
