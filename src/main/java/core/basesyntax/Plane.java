91ce6c5aec21486284a93f589ebbcfa4d22c9343
vosker <nikita.vaganov16@gmail.com>
2020-08-02 21:49:40 +0300

    solution added

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -7,5 +7,78 @@ package core.basesyntax;
  */
 
 public class Plane {
+    private String name;
+    private String color;
+    private int maxSpeed;
+    private int liftingCapacity;
+    private int maxHeight;
 
+    private Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.name;
+        this.color = planeBuilder.color;
+        this.maxSpeed = planeBuilder.maxSpeed;
+        this.liftingCapacity = planeBuilder.liftingCapacity;
+        this.maxHeight = planeBuilder.maxHeight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getLiftingCapacity() {
+        return liftingCapacity;
+    }
+
+    public int getMaxHeight() {
+        return maxHeight;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String color;
+        private int maxSpeed;
+        private int liftingCapacity;
+        private int maxHeight;
+
+        public PlaneBuilder(String name) {
+            this.name = name;
+        }
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setLiftingCapacity(int liftingCapacity) {
+            this.liftingCapacity = liftingCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setMaxHeight(int maxHeight) {
+            this.maxHeight = maxHeight;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
