4ae06497f59c5bc1d4229b3ade0d5c7dd1a31d7e
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-08-03 13:18:25 +0300

    Second commit. No bussiness logic in Main()

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,16 +2,5 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        Plane.PlaneBuilder planeBuilder = new Plane.PlaneBuilder("Boeing 737")
-                .setMass(350).setMaxMassTransported(70);
-        Plane plane1 = planeBuilder.setMaxHeight(12)
-                .setMaxSpeed(950)
-                .build();
-        Plane plane2 = planeBuilder.setMaxHeight(10)
-                .setMaxSpeed(1050)
-                .build();
-
-        System.out.println(plane1 + System.lineSeparator() + plane2);
     }
-
 }
