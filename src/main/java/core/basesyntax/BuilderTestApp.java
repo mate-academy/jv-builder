a116cb35ca3bedf011bc3d819a4fdaa9e08510e5
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-03 13:47:28 +0200

    Implement classes Plane and PlaneBuider

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane plane = new Plane.PlaneBuilder()
+                .setModel("Boing")
+                .setOrigin("Germany")
+                .setType("Militart")
+                .setMaxHeight(8000.0)
+                .setYear(2005)
+                .build();
+        System.out.println(plane.toString());
+    }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,65 @@
 package core.basesyntax;
 
 public class Plane {
+    private String model;
+    private String origin;
+    private String type;
+    private int year;
+    private double maxHeight;
 
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.origin = builder.origin;
+        this.type = builder.type;
+        this.year = builder.year;
+        this.maxHeight = builder.maxHeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{"
+                + "model='" + model + '\''
+                + ", origin='" + origin + '\''
+                + ", type='" + type + '\''
+                + ", year=" + year
+                + ", maxHeight=" + maxHeight
+                + '}';
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private String origin;
+        private String type;
+        private int year;
+        private double maxHeight;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setOrigin(String origin) {
+            this.origin = origin;
+            return this;
+        }
+
+        public PlaneBuilder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public PlaneBuilder setYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public PlaneBuilder setMaxHeight(double maxHeight) {
+            this.maxHeight = maxHeight;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
