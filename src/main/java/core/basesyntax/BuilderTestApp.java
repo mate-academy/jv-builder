28a9b949d76e9963d2b5f8c78e96dd6d592a8586
IvanLuchkin <id.luchkin@gmail.com>
2020-12-03 15:04:37 +0200

    Initial commit

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+
+    public static void main(String[] args) {
+        Plane firstTestPlane = new Plane.PlaneBuilder()
+                .setModel("Spitfire")
+                .setCompany("Supermarine")
+                .build();
+        System.out.println(firstTestPlane.toString());
+        Plane.PlaneBuilder planeBuilder = new Plane.PlaneBuilder().setCompany("North-American");
+        Plane mustang = planeBuilder.setModel("P-51 Mustang").setMaxSpeed(700).build();
+        System.out.println(mustang.toString());
+        Plane blackWidow = planeBuilder.setModel("P-61C Black Widow").setMaxSpeed(550).build();
+        System.out.println(blackWidow.toString());
+    }
 }
