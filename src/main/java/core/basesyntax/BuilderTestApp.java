dacb20e36c0ee52d785e928a950217ac44e89349
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-04 16:54:48 +0200

    1) my solution

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -2,6 +2,6 @@ package core.basesyntax;
 
 public class BuilderTestApp {
   public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
+
   }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,66 @@
 package core.basesyntax;
 
 public class Plane {
+    private String model;
+    private int height;
+    private int weight;
+    private int maxSpeed;
+    private int maxHeight;
 
+    public Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.height = builder.height;
+        this.weight = builder.weight;;
+        this.maxHeight = builder.maxHeight;
+        this.maxSpeed = builder.maxSpeed;
+    }
+
+
+    public static class PlaneBuilder{
+        private String model;
+        private int height;
+        private int weight;
+        private int maxSpeed;
+        private int maxHeight;
+
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setHeight(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(int weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setMaxHeight(int maxHeight) {
+            this.maxHeight = maxHeight;
+            return this;
+        }
+        public Plane build(){
+            return new Plane(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{" +
+                "model='" + model + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", maxSpeed=" + maxSpeed +
+                ", maxHeight=" + maxHeight +
+                '}';
+    }
 }
