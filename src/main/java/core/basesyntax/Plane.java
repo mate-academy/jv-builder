f7da9ac7a4bfbd2e729aeff3eba45d50479bf080
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-04 14:28:16 +0200

    Added a few fields to Plane class

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -2,4 +2,11 @@ package core.basesyntax;
 
 public class Plane {
 
+    private String name;
+    private String model;
+    private boolean isAirliner;
+    private int numberOfSits;
+    private int numberOfEngines;
+
+
 }
