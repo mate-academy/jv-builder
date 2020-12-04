9123ff02806b7f6e43e841091027cb307276713f
Igor Bochkor <igorbochkor@gmail.com>
2020-12-04 15:26:57 +0200

    solution

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane bestPlane = new Plane.PlaneBuilder().setType("military")
+                .setModel("f-16").setYear(2010).setSpeed(1234)
+                .setChassis("Chassis").setFuselage("full")
+                .build();
+
+        Plane voyager = new Plane.PlaneBuilder().setType("passanger")
+                .setModel("Boeing").setYear(2007).setSpeed(500)
+                .setChassis("chassis").setFuselage("Fuselage")
+                .build();
+        System.out.println(bestPlane.toString());
+        System.out.println(voyager.toString());
+    }
 }
