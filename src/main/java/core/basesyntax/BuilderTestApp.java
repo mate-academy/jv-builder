04a8cc36ef253b72a62b00ca6daeb63ed0e96312
OleksandrPokotylenko <aln.nko.of@gmail.com>
2020-12-05 01:05:22 +0200

    done

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane boeing747 = new Plane.PlaneBuilder().setModel("Boeing 747")
+                .setColor("Red with white stripes").setYear(1990)
+                .setPassengersCapacity(452).setEngineThrust(22.6).build();
+        System.out.println(boeing747.toString());
+    }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,64 @@
 package core.basesyntax;
 
 public class Plane {
+    private String model;
+    private String color;
+    private int year;
+    private int passengersCapacity;
+    private double engineThrust;
+
+    private Plane(PlaneBuilder planeBuilder) {
+        this.model = planeBuilder.model;
+        this.color = planeBuilder.color;
+        this.year = planeBuilder.year;
+        this.passengersCapacity = planeBuilder.passengersCapacity;
+        this.engineThrust = planeBuilder.engineThrust;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane : "
+                + "model = '" + model + '\'' + ", color = '" + color + '\''
+                + ", year = " + year + ", passengers capacity = " + passengersCapacity
+                + ", engine thrust = " + engineThrust;
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private String color;
+        private int year;
+        private int passengersCapacity;
+        private double engineThrust;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public PlaneBuilder setPassengersCapacity(int passengersCapacity) {
+            this.passengersCapacity = passengersCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setEngineThrust(double engineThrust) {
+            this.engineThrust = engineThrust;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 
 }
