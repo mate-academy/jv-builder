832275bfd8c5afe8c0746ef20f6197abf69718ef
Vitalik Kopach <apple_air@ukr.net>
2020-12-04 15:47:43 +0200

    implement pattern Builder in class Plane

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
+
   public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
+
+    Plane boeing747 = new Plane.PlaneBuilder()
+                    .setName("Boeing 747")
+                    .setAirPlaneType("passengers")
+                    .setYear(2018)
+                    .setCapacity(480)
+                    .build();
+
+    System.out.println(boeing747.toString());
+
+    Plane.PlaneBuilder airbus = new Plane.PlaneBuilder()
+                      .setColor("White")
+                      .setCelling(13000);
+
+    Plane airbusA330 = airbus.setName("Airbus A330")
+                      .setAirPlaneType("passengers")
+                      .setCapacity(246)
+                      .setRange(13450).build();
+
+    Plane airbusA330neo = airbus.setName("Airbus A330neo")
+                          .setAirPlaneType("passengers")
+                          .setCapacity(260)
+                          .setRange(15000)
+                          .build();
+
+    System.out.println(airbusA330.toString());
+    System.out.println(airbusA330neo.toString());
+
+
+
+
+
+
   }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -2,4 +2,87 @@ package core.basesyntax;
 
 public class Plane {
 
+        private String airPlaneType;
+        private String name;
+        private String color;
+        private int capacity;
+        private int range;
+        private int celling;
+        private int year;
+
+        private Plane(PlaneBuilder builder) {
+            this.airPlaneType = builder.airPlaneType;
+            this.name = builder.name;
+            this.color = builder.color;
+            this.capacity = builder.capacity;
+            this.range = builder.range;
+            this.celling = builder.celling;
+            this.year = builder.year;
+        }
+
+    public static class PlaneBuilder {
+            private String airPlaneType;
+            private String name;
+            private String color;
+            private int capacity;
+            private int range;
+            private int celling;
+            private int year;
+
+
+            public PlaneBuilder setCelling(int celling) {
+                this.celling = celling;
+                return this;
+            }
+
+            public PlaneBuilder setAirPlaneType(String airPlaneType) {
+                this.airPlaneType = airPlaneType;
+                return this;
+            }
+
+            public PlaneBuilder setColor(String color) {
+                this.color = color;
+                return this;
+            }
+
+            public PlaneBuilder setCapacity(int capacity) {
+                this.capacity = capacity;
+                return this;
+
+            }
+
+            public PlaneBuilder setRange(int range) {
+                this.range = range;
+                return this;
+
+            }
+
+            public PlaneBuilder setYear(int year) {
+                this.year = year;
+                return this;
+            }
+
+            public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+             }
+
+            public Plane build(){
+                return new Plane(this);
+            }
+        }
+
+    @Override
+    public String toString() {
+        return "Plane{" +
+                "airPlaneType='" + airPlaneType + '\'' +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", capacity=" + capacity +
+                ", range=" + range +
+                ", celling=" + celling +
+                ", year=" + year +
+                '}';
+    }
 }
+
