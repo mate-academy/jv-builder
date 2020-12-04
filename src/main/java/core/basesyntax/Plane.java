db6ab89d9527301a6e7a3bfb27d7e9738eec0c84
Tonitrus <grom4er@gmail.com>
2020-12-04 13:29:02 +0200

    Start create Fields, create static PlaneBuilder clas

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,38 @@
 package core.basesyntax;
 
 public class Plane {
+    private String engineType;
+    private String type;
+    private double enginePower;
+    private double maxSpeed;
+    private double maxHeight;
 
+    private Plane(PlaneBuilder builder) {
+
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getEnginePower() {
+        return enginePower;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public double getMaxHeight() {
+        return maxHeight;
+    }
+
+    public static class PlaneBuilder {
+
+
+    }
 }
