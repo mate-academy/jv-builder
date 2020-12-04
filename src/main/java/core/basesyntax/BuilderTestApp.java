346ac54e6b34107e842d2b5fcb28bc7293f72d99
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-04 13:45:33 +0200

    created PlaneBuilder

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane redPlane = new Plane.PlaneBuilder()
+                .setModel("passenger's")
+                .setMaxSpeed(1000)
+                .setColor("white")
+                .setWings(true)
+                .setAirframe(true)
+                .setPropeller(true)
+                .build();
+        System.out.println(redPlane.toString());
+    }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,80 @@
 package core.basesyntax;
 
 public class Plane {
+    private String model;
+    private String color;
+    private double maxSpeed;
+    private boolean propeller;
+    private boolean airframe;
+    private boolean wings;
 
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.color = builder.color;
+        this.maxSpeed = builder.maxSpeed;
+        this.propeller = builder.propeller;
+        this.airframe = builder.airframe;
+        this.wings = builder.wings;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("Builder{")
+                    + "model='" + model + '\''
+                    + ", color='" + color + '\''
+                    + ", maxSpeed=" + maxSpeed
+                    + ", propeller=" + propeller
+                    + ", airframe=" + airframe
+                    + ", wings=" + wings
+                    + '}';
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private String color;
+        private double maxSpeed;
+        private boolean propeller;
+        private boolean airframe;
+        private boolean wings;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public PlaneBuilder setMaxSpeed(double maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public PlaneBuilder setPropeller(boolean propeller) {
+            this.propeller = propeller;
+            return this;
+        }
+
+        public PlaneBuilder setAirframe(boolean airframe) {
+            this.airframe = airframe;
+            return this;
+        }
+
+        public PlaneBuilder setWings(boolean wings) {
+            this.wings = wings;
+            return this;
+        }
+
+        public Plane build() {
+            if (this.airframe == false
+                                 || this.propeller == false
+                                 || this.wings == false) {
+                throw new RuntimeException("Can't create Plane without airframe/propeller/wings");
+            }
+            return new Plane(this);
+        }
+
+    }
 }
