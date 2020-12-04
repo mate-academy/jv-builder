e80dc5200159fa828aec6eecc4afe43594618367
engineer027 <dp270597dma@gmail.com>
2020-12-04 16:19:05 +0200

    added solution

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -2,5 +2,7 @@ package core.basesyntax;
 
 public class BuilderTestApp {
     public static void main(String[] args) {
+        Plane plane = new Plane.PlaneBuilder().setColor("red").setModel("boeing").build();
+        System.out.println(plane.toString());
     }
 }
