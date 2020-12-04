cdd3c8c07f08152c411faf2a5435f170a5e9cab4
Alex <koleksandr4@gmail.com>
2020-12-04 15:23:23 +0200

    solved task

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,21 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane.PlaneBuilder planeBuilder = new Plane.PlaneBuilder();
+        Plane firstPlane = new Plane.PlaneBuilder()
+                .setName("Boeing")
+                .setModel("737-800")
+                .setColour("Green")
+                .setYear(1996)
+                .setPassengerCapacity(198)
+                .build();
+
+        Plane secondPlane = new Plane.PlaneBuilder()
+                .setName("Airbus")
+                .setModel("a380")
+                .build();
+
+        System.out.println(firstPlane.toString());
+    }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,67 @@
 package core.basesyntax;
 
 public class Plane {
+    private String name;
+    private String model;
+    private String colour;
+    private int year;
+    private int passengerCapacity;
 
+    private Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.name;
+        this.model = planeBuilder.model;
+        this.colour = planeBuilder.colour;
+        this.year = planeBuilder.year;
+        this.passengerCapacity = planeBuilder.passengerCapacity;
+    }
+
+    public static class PlaneBuilder {
+        private String name;
+        private String model;
+        private String colour;
+        private int year;
+        private int passengerCapacity;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setColour(String colour) {
+            this.colour = colour;
+            return this;
+        }
+
+        public PlaneBuilder setYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public PlaneBuilder setPassengerCapacity(int passengerCapacity) {
+            this.passengerCapacity = passengerCapacity;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{"
+                + "name='" + name + '\''
+                + ", model='" + model + '\''
+                + ", colour='" + colour + '\''
+                + ", year=" + year
+                + ", passengerCapacity="
+                + passengerCapacity + '}';
+    }
 }
+
+
