12eef6eb6b859f51f3292135019aaf089cb13687
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-04 15:28:25 +0200

    PlaneBuilder implemented

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        System.out.println(new Plane.PlaneBuilder().setAirliner(false).setName("AirBus")
+                .setModel("A320").setEnginePower(3000).setNumberOfEngines(4).setNumberOfSits(60)
+                .build());
+    }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -2,11 +2,74 @@ package core.basesyntax;
 
 public class Plane {
 
-    private String name;
-    private String model;
-    private boolean isAirliner;
-    private int numberOfSits;
-    private int numberOfEngines;
+    private final String name;
+    private final String model;
+    private final boolean isAirliner;
+    private final int numberOfSits;
+    private final int numberOfEngines;
+    private final int enginePower;
 
+    private Plane(PlaneBuilder planeBuilder) {
+        this.name = planeBuilder.name;
+        this.model = planeBuilder.model;
+        this.isAirliner = planeBuilder.isAirliner;
+        this.numberOfSits = planeBuilder.numberOfSits;
+        this.numberOfEngines = planeBuilder.numberOfEngines;
+        this.enginePower = planeBuilder.enginePower;
+    }
 
+    @Override
+    public String toString() {
+        return "Plane{"
+                + "name='" + name + '\''
+                + ", model='" + model + '\''
+                + ", isAirliner=" + isAirliner
+                + ", numberOfSits=" + numberOfSits
+                + ", numberOfEngines=" + numberOfEngines
+                + ", enginePower=" + enginePower + '}';
+    }
+
+    public static class PlaneBuilder {
+
+        private String name;
+        private String model;
+        private boolean isAirliner;
+        private int numberOfSits;
+        private int numberOfEngines;
+        private int enginePower;
+
+        public PlaneBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setAirliner(boolean airliner) {
+            this.isAirliner = airliner;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfSits(int numberOfSits) {
+            this.numberOfSits = numberOfSits;
+            return this;
+        }
+
+        public PlaneBuilder setNumberOfEngines(int numberOfEngines) {
+            this.numberOfEngines = numberOfEngines;
+            return this;
+        }
+
+        public PlaneBuilder setEnginePower(int enginePower) {
+            this.enginePower = enginePower;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
