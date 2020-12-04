b05e5f293ace0697e5e5049a013b413141b0d02b
Serhii Semenykhin <semyonich@users.noreply.github.com>
2020-12-04 14:13:46 +0200

    PlaneBuilder implemented, tests passed

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane plane = new Plane.PlaneBuilder().setCompany("Boeing").setName("B-737")
+                .setLength(50).setSeats(150).setYear(2020).build();
+        System.out.println(plane.toString());
+    }
 }
