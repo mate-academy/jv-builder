e326d41960bb82e39f3c94a568ce7337f165fe21
Vitalik Kopach <apple_air@ukr.net>
2020-12-04 16:01:40 +0200

    fixed code style

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -2,39 +2,33 @@ package core.basesyntax;
 
 public class BuilderTestApp {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Plane boeing747 = new Plane.PlaneBuilder()
-                    .setName("Boeing 747")
-                    .setAirPlaneType("passengers")
-                    .setYear(2018)
-                    .setCapacity(480)
-                    .build();
+        Plane boeing747 = new Plane.PlaneBuilder()
+                .setName("Boeing 747")
+                .setAirPlaneType("passengers")
+                .setYear(2018)
+                .setCapacity(480)
+                .build();
 
-    System.out.println(boeing747.toString());
+        System.out.println(boeing747.toString());
 
-    Plane.PlaneBuilder airbus = new Plane.PlaneBuilder()
-                      .setColor("White")
-                      .setCelling(13000);
+        Plane.PlaneBuilder airbus = new Plane.PlaneBuilder()
+                .setColor("White")
+                .setCelling(13000);
 
-    Plane airbusA330 = airbus.setName("Airbus A330")
-                      .setAirPlaneType("passengers")
-                      .setCapacity(246)
-                      .setRange(13450).build();
+        Plane airbusA330 = airbus.setName("Airbus A330")
+                 .setAirPlaneType("passengers")
+                 .setCapacity(246)
+                 .setRange(13450).build();
 
-    Plane airbusA330neo = airbus.setName("Airbus A330neo")
-                          .setAirPlaneType("passengers")
-                          .setCapacity(260)
-                          .setRange(15000)
-                          .build();
+        Plane airbusA330neo = airbus.setName("Airbus A330neo")
+                  .setAirPlaneType("passengers")
+                  .setCapacity(260)
+                  .setRange(15000)
+                  .build();
 
-    System.out.println(airbusA330.toString());
-    System.out.println(airbusA330neo.toString());
-
-
-
-
-
-
-  }
+        System.out.println(airbusA330.toString());
+        System.out.println(airbusA330neo.toString());
+    }
 }
