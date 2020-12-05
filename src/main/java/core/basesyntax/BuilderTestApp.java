ee9873619f9213144b31b477a8903ff81f9c2cc3
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-05 23:59:49 +0200

    1)Commit

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane plane = new Plane.PlaneBuilder().setModel("Boeing737")
+                                  .setColor("white").setLength(30)
+                                  .setWide(28).setPrice(90).build();
+
+        System.out.println(plane.toString());
+    }
 }
