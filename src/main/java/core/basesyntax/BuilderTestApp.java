291c75ba81b257a8a6fe308a140cec96d6c7efd2
Alekearl <alexandrhurko@gmail.com>
2020-12-05 17:48:23 +0200

    all tests passed during BuilderTestApp work

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+
+        Plane.PlaneBuilder boeingSevenFourSeven = new Plane.PlaneBuilder()
+                .setName("Boeing")
+                .setModel("747");
+
+        Plane boeing = boeingSevenFourSeven
+                .setYearBuild(2010)
+                .setEngineHorsePower(5000)
+                .setPassengersCapacity(500)
+                .build();
+    }
 }
