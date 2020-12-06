4c96f5065d970f1a1790d563c66f57f64fad2fe7
egorka <entertainment.veks@gmail.com>
2020-12-06 11:28:38 +0200

    yegor sereda 1st commit

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -2,6 +2,19 @@ package core.basesyntax;
 
 public class BuilderTestApp {
   public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
+      Plane firstPlane = new Plane.Builder()
+              .setColor("Gray")
+              .setDoors(4)
+              .setSits(64)
+              .setSize("Big")
+              .build();
+
+      Plane secondPlane = new Plane.Builder()
+              .setDoors(6)
+              .setCargo(true)
+              .build();
+
+    System.out.println(firstPlane.toString());
+    System.out.println(secondPlane.toString());
   }
 }
