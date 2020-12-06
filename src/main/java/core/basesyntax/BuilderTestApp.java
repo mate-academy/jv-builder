f169fb778d3f7573af766b7ff2cb7952ee4ebdf0
Elena Samsonova <elenasamsonova08@gmail.com>
2020-12-06 20:50:01 +0200

    builder task done

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+
+        Plane boeing747 = new Plane.PlaneBuilder()
+                .setModel("Boeing 747")
+                .setCompany("UIA")
+                .setYear(2015)
+                .setCapacity(70)
+                .setColour("blue and yellow")
+                .build();
+
+        System.out.println(boeing747.toString());
+    }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,66 @@
 package core.basesyntax;
 
 public class Plane {
+    private String model;
+    private String company;
+    private int year;
+    private int capacity;
+    private String colour;
+
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.company = builder.company;
+        this.year = builder.year;
+        this.capacity = builder.capacity;
+        this.colour = builder.colour;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{"
+                + "model='" + model + '\''
+                + ", company='" + company + '\''
+                + ", year=" + year
+                + ", capacity=" + capacity
+                + ", colour='" + colour + '\''
+                + '}';
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private String company;
+        private int year;
+        private int capacity;
+        private String colour;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setCompany(String company) {
+            this.company = company;
+            return this;
+        }
+
+        public PlaneBuilder setYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public PlaneBuilder setCapacity(int capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        public PlaneBuilder setColour(String colour) {
+            this.colour = colour;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 
 }
