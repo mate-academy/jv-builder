b22de5525d50b757b7f02aa8995b57133e6408fc
kerrender <agreed_note@hotmail.com>
2020-12-06 18:34:48 +0200

    Initial

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane plane = new Plane.PlaneBuilder().setModel("AN-51").setColor("White")
+                .setYear(2000).setLength(20).setMission("Charter").build();
+        System.out.println(plane.toString());
+    }
 }
