3896a2f911958c046875b624e3edb8c30286426e
Sashk52 <a.tsven@gmail.com>
2020-12-07 00:01:29 +0200

    added solution

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane plane = new Plane.PlaneBuilder()
+                .setMaker("Airbus")
+                .setModel("A321")
+                .setEnginePower(15000)
+                .setPassegerCapacity(220)
+                .setMaxFlightHigh(5950)
+                .build();
+
+        System.out.println(plane.toString());
+    }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,65 @@
 package core.basesyntax;
 
 public class Plane {
+    private String maker;
+    private String model;
+    private int enginePower;
+    private int passegerCapacity;
+    private int maxFlightHigh;
 
+    private Plane(PlaneBuilder builder) {
+        this.maker = builder.maker;
+        this.model = builder.model;
+        this.enginePower = builder.enginePower;
+        this.passegerCapacity = builder.passegerCapacity;
+        this.maxFlightHigh = builder.maxFlightHigh;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{"
+                + "maker='" + maker + '\''
+                + ", model='" + model + '\''
+                + ", enginePower=" + enginePower
+                + ", passegerCapacity=" + passegerCapacity
+                + ", maxFlightHigh=" + maxFlightHigh
+                + '}';
+    }
+
+    public static class PlaneBuilder {
+        private String maker;
+        private String model;
+        private int enginePower;
+        private int passegerCapacity;
+        private int maxFlightHigh;
+
+        public PlaneBuilder setMaker(String maker) {
+            this.maker = maker;
+            return this;
+        }
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setEnginePower(int enginePower) {
+            this.enginePower = enginePower;
+            return this;
+        }
+
+        public PlaneBuilder setPassegerCapacity(int passegerCapacity) {
+            this.passegerCapacity = passegerCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setMaxFlightHigh(int maxFlightHigh) {
+            this.maxFlightHigh = maxFlightHigh;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
