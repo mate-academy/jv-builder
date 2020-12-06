506be3780cfec97bdf034c7cb4ebabe09b2d2cab
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-06 10:46:17 +0200

    first commit

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+
+    public static void main(String[] args) {
+        Plane.PlaneBuilder planeBuilder = new Plane.PlaneBuilder()
+                .setModel("Airbus 383")
+                .setFlyDistance(1500)
+                .setPassengersCapacity(118)
+                .setIsIntercontinental(true)
+                .setHasBusinessClass(false);
+
+        Plane plane = planeBuilder.build();
+
+        plane.toString();
+    }
 }
--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,64 @@
 package core.basesyntax;
 
 public class Plane {
+    private String model;
+    private int flyDistance;
+    private int passengersCapacity;
+    private boolean isIntercontinental;
+    private boolean hasBusinessClass;
 
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.flyDistance = builder.flyDistance;
+        this.passengersCapacity = builder.passengersCapacity;
+        this.isIntercontinental = builder.isIntercontinental;
+        this.hasBusinessClass = builder.hasBusinessClass;
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private int flyDistance;
+        private int passengersCapacity;
+        private boolean isIntercontinental;
+        private boolean hasBusinessClass;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setFlyDistance(int flyDistance) {
+            this.flyDistance = flyDistance;
+            return this;
+        }
+
+        public PlaneBuilder setPassengersCapacity(int passengersCapacity) {
+            this.passengersCapacity = passengersCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setIsIntercontinental(boolean isIntercontinental) {
+            this.isIntercontinental = isIntercontinental;
+            return this;
+        }
+
+        public PlaneBuilder setHasBusinessClass(boolean hasBusinessClass) {
+            this.hasBusinessClass = hasBusinessClass;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{"
+                + "model='" + model + '\''
+                + ", flyDistance=" + flyDistance
+                + ", passengersCapacity=" + passengersCapacity
+                + ", isIntercontinental=" + isIntercontinental
+                + ", hasBusinessClass=" + hasBusinessClass + '}';
+    }
 }
