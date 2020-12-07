8c95cca56b997137bca790ab425c42300f5cb533
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-07 09:53:37 +0200

    fixed checkstyle

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,9 +1,9 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      Plane plane = new Plane.PlaneBuilder().setModel("Airbus A320").setName("MAU")
-              .setHorsePower(25_000).setSeats(150).setYear(1987).build();
-      System.out.println(plane.toString());
-  }
+    public static void main(String[] args) {
+        Plane plane = new Plane.PlaneBuilder().setModel("Airbus A320").setName("MAU")
+                .setHorsePower(25_000).setSeats(150).setYear(1987).build();
+        System.out.println(plane.toString());
+    }
 }
