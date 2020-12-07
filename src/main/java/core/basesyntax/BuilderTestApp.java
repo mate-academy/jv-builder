614cd0913c1bff480cd6f45b3728ee113fac8a05
m.kislukhina <marharita.kislukhina@gmail.com>
2020-12-07 08:46:45 +0100

    All fields sated for with the builder.

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -3,7 +3,8 @@ package core.basesyntax;
 public class BuilderTestApp {
 
     public static void main(String[] args) {
-        Plane boeing = new Plane.PlaneBuilder().setColor("pink").build();
+        Plane boeing = new Plane.PlaneBuilder().setColor("pink").setModel("123")
+                .setSpeed(500).setNumberOfSeats(75).setYearOfConstruction(2012).build();
     }
 }
 
