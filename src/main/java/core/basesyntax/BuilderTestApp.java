1e3e4e21f4184b6a851057158239844c3c0c33f4
tbondare <tanya.bon2016@gmail.com>
2020-12-07 18:17:02 +0200

    solution was added firstly

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane plane = new Plane.PlaneBuilder()
+                .setNameCompany("AirLines")
+                .setModel("TU-89")
+                .setPower(20000)
+                .setIsFirstClass(false)
+                .setCountryConnect("Poland")
+                .build();
+        System.out.println(plane.toString());
+    }
 }
