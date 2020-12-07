ef1a653d20c00f77e1338057b3cdf77a74cb20d5
Resci <vIsUAl123>
2020-12-07 02:02:31 +0200

    Implement class Plane and PlaneBuilder according to Builder pattern and write tests in BuilderTestApp.java

--- src/main/java/core/basesyntax/Plane.java
+++ src/main/java/core/basesyntax/Plane.java
@@ -1,5 +1,74 @@
 package core.basesyntax;
 
 public class Plane {
+    private String model;
+    private float weight;
+    private float weightCapacity;
+    private int peopleCapacity;
+    private int crewSize;
+    private int year;
 
+    private Plane(PlaneBuilder builder) {
+        this.model = builder.model;
+        this.weight = builder.weight;
+        this.weightCapacity = builder.weightCapacity;
+        this.peopleCapacity = builder.peopleCapacity;
+        this.crewSize = builder.crewSize;
+        this.year = builder.year;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{"
+                + "model='" + model + '\''
+                + ", weight=" + weight
+                + ", weightCapacity=" + weightCapacity
+                + ", peopleCapacity=" + peopleCapacity
+                + ", crewSize=" + crewSize
+                + ", year=" + year
+                + '}';
+    }
+
+    public static class PlaneBuilder {
+        private String model;
+        private float weight;
+        private float weightCapacity;
+        private int peopleCapacity;
+        private int crewSize;
+        private int year;
+
+        public PlaneBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PlaneBuilder setWeight(float weight) {
+            this.weight = weight;
+            return this;
+        }
+
+        public PlaneBuilder setWeightCapacity(float weightCapacity) {
+            this.weightCapacity = weightCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setPeopleCapacity(int peopleCapacity) {
+            this.peopleCapacity = peopleCapacity;
+            return this;
+        }
+
+        public PlaneBuilder setCrewSize(int crewSize) {
+            this.crewSize = crewSize;
+            return this;
+        }
+
+        public PlaneBuilder setYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public Plane build() {
+            return new Plane(this);
+        }
+    }
 }
