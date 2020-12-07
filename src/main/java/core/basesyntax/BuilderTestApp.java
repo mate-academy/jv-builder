5abb3d016e1c9832c9c1c38ec5e04709a17d048a
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-07 09:52:37 +0200

    mentor sugestion: deleted Plane.PlaneBuilder var and leave just Plane var

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -3,15 +3,13 @@ package core.basesyntax;
 public class BuilderTestApp {
 
     public static void main(String[] args) {
-        Plane.PlaneBuilder planeBuilder = new Plane.PlaneBuilder()
+        Plane plane = new Plane.PlaneBuilder()
                 .setModel("Airbus 383")
                 .setFlyDistance(1500)
                 .setPassengersCapacity(118)
                 .setIsIntercontinental(true)
-                .setHasBusinessClass(false);
-
-        Plane plane = planeBuilder.build();
-
+                .setHasBusinessClass(false)
+                .build();
         plane.toString();
     }
 }
