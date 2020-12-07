49e7227846160d4668de0ecd2a5a42e16f21551c
kerrender <agreed_note@hotmail.com>
2020-12-07 10:48:05 +0200

    Builder method calling refactored

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -2,8 +2,13 @@ package core.basesyntax;
 
 public class BuilderTestApp {
     public static void main(String[] args) {
-        Plane plane = new Plane.PlaneBuilder().setModel("AN-51").setColor("White")
-                .setYear(2000).setLength(20).setMission("Charter").build();
+        Plane plane = new Plane.PlaneBuilder()
+                .setModel("AN-51")
+                .setColor("White")
+                .setYear(2000)
+                .setLength(20)
+                .setMission("Charter")
+                .build();
         System.out.println(plane.toString());
     }
 }
