d457d9b83eb8e3f8d4b56c969800ce1dd50172eb
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-07 09:43:33 +0200

    Local variable Plane somePlane is introduced to BuilderTestApp class

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -2,8 +2,9 @@ package core.basesyntax;
 
 public class BuilderTestApp {
     public static void main(String[] args) {
-        System.out.println(new Plane.PlaneBuilder().setIsAirliner(false).setName("AirBus")
+        Plane somePlane = new Plane.PlaneBuilder().setIsAirliner(false).setName("AirBus")
                 .setModel("A320").setEnginePower(3000).setNumberOfEngines(4).setNumberOfSits(60)
-                .build());
+                .build();
+        System.out.println(somePlane);
     }
 }
