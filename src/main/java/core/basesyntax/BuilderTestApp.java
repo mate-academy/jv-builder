e706c3ca3aee9c26c1d68d2e3330f234547f1b9a
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-07 10:56:01 +0200

    2)Commit - now each builder method called from new line

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -2,9 +2,12 @@ package core.basesyntax;
 
 public class BuilderTestApp {
     public static void main(String[] args) {
-        Plane plane = new Plane.PlaneBuilder().setModel("Boeing737")
-                                  .setColor("white").setLength(30)
-                                  .setWide(28).setPrice(90).build();
+        Plane.PlaneBuilder plane = new Plane.PlaneBuilder().setModel("Boeing737");
+        plane = plane.setColor("white");
+        plane = plane.setLength(30);
+        plane = plane.setWide(28);
+        plane = plane.setPrice(90);
+        plane.build();
 
         System.out.println(plane.toString());
     }
