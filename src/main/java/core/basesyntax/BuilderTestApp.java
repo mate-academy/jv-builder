58a7d4919f92501948f3e570cf79e69c406909a0
vsak <vsak@itsmartflex.com>
2021-03-15 18:06:46 +0200

    Solution

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-      // Remove this comment and test your Builder implementation here...
-  }
+    public static void main(String[] args) {
+        Plane yellowFastPlane = new Plane.PlaneBuilder()
+                .setColor("yellow")
+                .setEngineVolume(4000)
+                .setModel("TY-102")
+                .setFirm("Ukraine Air Lines")
+                .build();
+
+        System.out.println(yellowFastPlane.toString());
+    }
 }
