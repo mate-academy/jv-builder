a28d9c63f7ae113ad9cc47386e452ae829f85502
vsak <vsak@itsmartflex.com>
2021-03-23 18:41:05 +0200

    Fixed indentation

--- src/main/java/core/basesyntax/BuilderTestApp.java
+++ src/main/java/core/basesyntax/BuilderTestApp.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 public class BuilderTestApp {
-  public static void main(String[] args) {
-    Plane yellowFastPlane = new Plane.PlaneBuilder()
-            .setColor("yellow")
-            .setEngineVolume(4000)
-            .setModel("TY-102")
-            .setFirm("Ukraine Air Lines")
-            .build();
+    public static void main(String[] args) {
+        Plane yellowFastPlane = new Plane.PlaneBuilder()
+                .setColor("yellow")
+                .setEngineVolume(4000)
+                .setModel("TY-102")
+                .setFirm("Ukraine Air Lines")
+                .build();
 
-    System.out.println(yellowFastPlane.toString());
-  }
+        System.out.println(yellowFastPlane.toString());
+    }
 }
